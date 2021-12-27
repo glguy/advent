5fb342350a89c0b63e7398d4314e326ff8e70493
Eric Mertens <emertens@galois.com>
2021-12-27 11:03:39 -0600

    6

--- solutions/src/2015/06.hs
+++ solutions/src/2015/06.hs
@@ -1,44 +1,46 @@
+{-# LANGUAGE BlockArguments #-}
 module Main where
 
+import Advent ( getInputLines, countBy )
+import Advent.Coord ( Coord(..) )
+import Control.Monad.ST ( ST, runST )
 import Data.Array.ST
-import Data.Foldable
-import Control.Monad.ST
+import Data.Foldable ( for_, traverse_ )
 
 data Op = On | Off | Toggle
-type Point = (Int,Int)
-data Command = Command !Op Point Point
+data Command = Command !Op Coord Coord
 
 main :: IO ()
 main =
-  do cmds <- loadInput
-     print (runST (part1 cmds))
-     print (runST (part2 cmds))
+ do cmds <- map parseLine <$> getInputLines 6
+    print (runST (part1 cmds))
+    print (runST (part2 cmds))
 
 part1 :: [Command] -> ST s Int
 part1 cmds =
-  do a <- newBitGrid
-     traverse_ (bitCommand a) cmds
-     xs <- getElems a
-     return $! length (filter id xs)
+ do a <- newBitGrid
+    traverse_ (bitCommand a) cmds
+    xs <- getElems a
+    return $! countBy id xs
 
 part2 :: [Command] -> ST s Int
 part2 cmds =
-  do a <- newIntGrid
-     traverse_ (intCommand a) cmds
-     xs <- getElems a
-     return $! sum xs
+ do a <- newIntGrid
+    traverse_ (intCommand a) cmds
+    xs <- getElems a
+    return $! sum xs
 
-bitCommand :: STUArray s Point Bool -> Command -> ST s ()
+bitCommand :: STUArray s Coord Bool -> Command -> ST s ()
 bitCommand a (Command op x y) =
-  forRange x y $ \p ->
+  for_ (range (x, y)) \p ->
     case op of
       On     -> writeArray a p True
       Off    -> writeArray a p False
       Toggle -> writeArray a p . not =<< readArray a p
 
-intCommand :: STUArray s Point Int -> Command -> ST s ()
+intCommand :: STUArray s Coord Int -> Command -> ST s ()
 intCommand a (Command op x y) =
-  forRange x y $ \p ->
+  for_ (range (x, y)) \p ->
     writeArray a p . upd =<< readArray a p
   where
   upd = case op of
@@ -46,21 +48,11 @@ intCommand a (Command op x y) =
           Off    -> max 0 . subtract 1
           Toggle -> (+2)
 
-forRange :: Applicative m => Point -> Point -> (Point -> m a) -> m ()
-forRange (xlo,ylo) (xhi,yhi) k =
-  for_ [xlo..xhi] $ \x ->
-  for_ [ylo..yhi] $ \y ->
-  k (x,y)
-{-# INLINE forRange #-}
+newBitGrid :: ST s (STUArray s Coord Bool)
+newBitGrid = newArray (C 0 0, C 999 999) False
 
-loadInput :: IO [Command]
-loadInput = map parseLine . lines <$> readFile "input6.txt"
-
-newBitGrid :: ST s (STUArray s Point Bool)
-newBitGrid = newArray ((0,0),(999,999)) False
-
-newIntGrid :: ST s (STUArray s Point Int)
-newIntGrid = newArray ((0,0),(999,999)) 0
+newIntGrid :: ST s (STUArray s Coord Int)
+newIntGrid = newArray (C 0 0, C 999 999) 0
 
 parseLine :: String -> Command
 parseLine str = Command op (parsePoint p1) (parsePoint p2)
@@ -72,9 +64,9 @@ parseLine str = Command op (parsePoint p1) (parsePoint p2)
       ["toggle",    x,"through",y] -> (Toggle, x, y)
       _ -> error ("Bad line: " ++ str)
 
-parsePoint :: String -> Point
+parsePoint :: String -> Coord
 parsePoint str =
   foldr const (error ("Bad point: " ++ str)) $
   do (x,',':ystr) <- reads str
      (y,[]      ) <- reads ystr
-     return (x,y)
+     return (C y x)
