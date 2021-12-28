77b1befa225d332442dfacbf26ba9d9cb8d35278
Eric Mertens <emertens@gmail.com>
2021-12-28 10:16:08 -0600

    Update 04.hs

--- solutions/src/2017/04.hs
+++ solutions/src/2017/04.hs
@@ -19,14 +19,6 @@ main =
      print (countBy allUniqueModuloAnagrams input)
 
 
--- | Parse input as lines of words.
---
--- >>> parseInput "one two three\nfour five six\n"
--- [["one","two","three"],["four","five","six"]]
-parseInput :: String -> [[String]]
-parseInput = map words . lines
-
-
 -- | Predicate that returns true when all elements in the list are unique
 --
 -- >>> allUnique (words "aa bb cc dd ee")
