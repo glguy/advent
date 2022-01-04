1b5d34fe0775a4a17dc6f28bf5f2695fae7a4b84
Eric Mertens <emertens@galois.com>
2022-01-04 11:08:36 -0800

    10

--- solutions/src/2019/10.hs
+++ solutions/src/2019/10.hs
@@ -18,10 +18,9 @@ module Main (main) where
 
 import Advent (getInputLines, countBy)
 import Advent.Coord (coordLines, Coord(..), manhattan, scaleCoord)
-import Data.Foldable (Foldable(toList), maximumBy)
+import Data.Foldable (Foldable(toList))
 import Data.List (sortOn, transpose)
 import Data.Map qualified as Map
-import Data.Ord (comparing)
 import Data.Ratio ((%))
 import Data.Set (Set)
 import Data.Set qualified as Set
@@ -32,13 +31,18 @@ import Data.Set qualified as Set
 main :: IO ()
 main =
  do inp <- getInputLines 10
-    let m = Set.fromList [c | (c,'#') <- coordLines inp]
-
-    let (base, vis) = maximumBy (comparing snd) [(i, countBy (lineOfSight m i) m) | i <- toList m]
+    let locs = Set.fromList [c | (c,'#') <- coordLines inp]
+    let (vis, base) = findBase locs
     print vis
-    let C y x = spiral base m !! 200
+    let C y x = spiral base (Set.delete base locs) !! 199
     print (x * 100 + y)
 
+-- | Find the location of the base with the most visibility.
+findBase :: Set Coord -> (Int, Coord)
+findBase m = maximum [(countBy (lineOfSight m i) m, i) | i <- toList m]
+
+-- | Return the coordinates ordered in a spiral given a center
+-- location.
 spiral :: Coord -> Set Coord -> [Coord]
 spiral base
   = concat
@@ -47,10 +51,28 @@ spiral base
   . groupOn (toAngle . subtract base)
   . Set.toList
 
-groupOn :: Ord b => (a -> b) -> [a] -> [[a]]
-groupOn f xs = Map.elems (Map.fromListWith (++) [(f x, [x]) | x <- xs])
+-- | Check if two asteroids have line of sight between each other.
+lineOfSight :: Set Coord -> Coord -> Coord -> Bool
+lineOfSight ast a b = a /= b && and [Set.notMember c ast | c <- between a b]
+
+-- * Coordinate utilities
 
 -- | Return all the locations that fall on a line between two coordinates.
+--
+-- >>> between (C 1 1) (C 7 4)
+-- [C 3 2,C 5 3]
+--
+-- >>> between (C 1 1) (C 6 6)
+-- [C 2 2,C 3 3,C 4 4,C 5 5]
+--
+-- >>> between (C 5 5) (C 1 1)
+-- [C 4 4,C 3 3,C 2 2]
+--
+-- >>> between (C 1 1) (C 6 7)
+-- []
+--
+-- >>> between (C 0 0) (C 0 0)
+-- []
 between :: Coord -> Coord -> [Coord]
 between a b = [a + scaleCoord i unit | i <- [1 .. n-1]]
   where
@@ -58,22 +80,42 @@ between a b = [a + scaleCoord i unit | i <- [1 .. n-1]]
     n = gcd dx dy
     unit = C (dy `quot` n) (dx `quot` n)
 
--- | Check if two asteroids have line of sight between each other.
-lineOfSight :: Set Coord -> Coord -> Coord -> Bool
-lineOfSight ast a b = a /= b && and [Set.notMember c ast | c <- between a b]
-
--- | Angles ordered starting from pointing up and proceeding
--- clockwise.
-data Angle = Angle !Int !Rational -- quadrant and slope
-  deriving (Eq, Ord, Show)
-
--- | Compute the angle from the origin to a given coordinate.
-toAngle :: Coord -> Angle
+-- | Compute a representation of the the angle from the origin to the given
+-- coordinate such that the angles are ordered clockwise starting at directly
+-- north.
+--
+-- >>> import Advent.Coord
+-- >>> import Data.List (sort)
+-- >>> let clockwise = [origin, north, north+north+east, north+east, north+east+east, east, south+east+east, south+east, south+south+east, south, south+south+west, south+west, south+west+west, west, north+west+west, north+west, north+north+west]
+-- >>> let angles = map toAngle clockwise
+-- >>> angles
+-- [(-1) % 1,0 % 1,1 % 3,1 % 2,2 % 3,1 % 1,4 % 3,3 % 2,5 % 3,2 % 1,7 % 3,5 % 2,8 % 3,3 % 1,10 % 3,7 % 2,11 % 3]
+--
+-- Angles have clockwise arrangement:
+--
+-- >>> sort angles == angles
+-- True
+--
+-- Angles are independent of scale:
+--
+-- >>> map toAngle clockwise == map (toAngle . (3*)) clockwise
+-- True
+toAngle :: Coord -> Rational
 toAngle (C y x)
-  | x == 0, y == 0 = Angle 0 0
-  | x >= 0, y <  0 = mk 1 x (-y)    -- upper right
-  | x >  0, y >= 0 = mk 2 y x       -- lower right
-  | x <= 0, y >  0 = mk 3 (-x) y    -- lower left
-  | otherwise      = mk 4 (-y) (-x) -- upper left
+  | x == 0, y == 0 = -1             -- center
+  | x >= 0, y <  0 = mk 1 x (-y)    -- northeast
+  | x >  0, y >= 0 = mk 2 y x       -- southeast
+  | x <= 0, y >  0 = mk 3 (-x) y    -- southwest
+  | otherwise      = mk 4 (-y) (-x) -- northwest
   where
-    mk i a b = Angle i (fromIntegral a % fromIntegral b)
+    -- q in [1,2,3,4]; a >= 0; b > 0
+    mk q a b = q - fromIntegral b % fromIntegral (a+b)
+
+-- * List utilities
+
+-- | Group the elements of a list using a charactizing function.
+--
+-- >>> groupOn (`mod` 3) [0..10]
+-- [[9,6,3,0],[10,7,4,1],[8,5,2]]
+groupOn :: Ord b => (a -> b) -> [a] -> [[a]]
+groupOn f xs = Map.elems (Map.fromListWith (++) [(f x, [x]) | x <- xs])
