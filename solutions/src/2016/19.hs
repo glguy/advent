e68ca4c61d5c79b4e1e6f716b7dd7c59b1297de6
Eric Mertens <emertens@gmail.com>
2022-01-05 19:55:12 -0800

    19

--- solutions/src/2016/19.hs
+++ solutions/src/2016/19.hs
@@ -1,4 +1,4 @@
-{-# Language ImportQualifiedPost #-}
+{-# Language ImportQualifiedPost, QuasiQuotes #-}
 {-|
 Module      : Main
 Description : Day 19 solution
@@ -8,38 +8,32 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2016/day/19>
 
+Elves sitting in a circle stealing presents.
+
 -}
 module Main where
 
+import Advent (format)
 import Data.Sequence (Seq)
 import Data.Sequence qualified as Seq
 
-elves :: Int
-elves = 3012210
-
+-- | >>> :main
+-- 1830117
+-- 1417887
 main :: IO ()
 main =
-  do print (part1 [1..elves])
-     print (part2 [1..elves])
+ do elves <- [format|2016 19 %u%n|]
+    let ring = Seq.fromList [1..elves]
+    print (part1 ring)
+    print (part2 ring)
 
-part1 :: [a] -> Maybe a
-part1 = part1' []
+part1 :: Seq a -> a
+part1 (x Seq.:<| Seq.Empty) = x
+part1 (x Seq.:<| _ Seq.:<| xs) = part1 (xs Seq.|> x)
 
-part1' :: [a] -> [a] -> Maybe a
-part1' [] []       = Nothing
-part1' [] [x]      = Just x
-part1' ys (x:_:xs) = part1' (x:ys) xs
-part1' ys xs       = part1' [] (xs ++ reverse ys)
-
-part2 :: [a] -> Maybe a
-part2 = part2' . Seq.fromList
-
-part2' :: Seq a -> Maybe a
-part2' xs =
-  case Seq.viewl xs of
-    Seq.EmptyL -> Nothing
-    x Seq.:< ys
-      | Seq.null ys -> Just x
-      | otherwise   -> part2' ((l Seq.>< Seq.drop 1 r) Seq.|> x)
-          where (l,r) = Seq.splitAt (half (length ys)) ys
-                half x = (x-1) `div` 2
+part2 :: Seq a -> a
+part2 (x Seq.:<| Seq.Empty) = x
+part2 (x Seq.:<| xs) = part2 (xs' Seq.|> x)
+  where
+    xs' = Seq.deleteAt (half (length xs)) xs
+    half x = (x-1) `quot` 2
