2bc253744512b797a17a458c1842c5c2bfa7824f
Eric Mertens <emertens@gmail.com>
2022-01-07 20:12:14 -0800

    15

--- solutions/src/2016/15.hs
+++ solutions/src/2016/15.hs
@@ -8,6 +8,8 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2016/day/15>
 
+Solved using <https://en.wikipedia.org/wiki/Chinese_remainder_theorem>
+
 -}
 module Main where
 
@@ -15,12 +17,27 @@ import Advent (format)
 import Advent.Chinese (chinese, toMod)
 
 -- | >>> :main
--- 376777
--- 3903937
+-- Just 376777
+-- Just 3903937
 main :: IO ()
 main =
- do input <- [format|2016 15 (Disc #%lu has %lu positions; at time=%lu, it is at position %lu.%n)*|]
-    let input1 = [toMod (-p-i+t) n | (i, n, t, p) <- input] 
-    let input2 = input1 ++ [toMod (-fromIntegral (length input1)-1) 11]
-    print (chinese input1)
-    print (chinese input2)
+ do input1 <- [format|2016 15 (Disc #%lu has %lu positions; at time=%lu, it is at position %lu.%n)*|]
+    let input2 = input1 ++ [(fromIntegral (length input1) + 1, 11, 0, 0)]
+    print (solve input1)
+    print (solve input2)
+
+-- | Given a list of discs, find the right time to push the button.
+--
+-- Example:
+--
+-- @
+-- Disc #1 has 5 positions; at time=0, it is at position 4.
+-- Disc #2 has 2 positions; at time=0, it is at position 1.
+-- @
+--
+-- >>> solve [(1, 5, 0, 4), (2, 2, 0, 1)]
+-- Just 5
+solve ::
+  [(Integer, Integer, Integer, Integer)] {- ^ disc location, disc size, initial time, initial rotations -} ->
+  Maybe Integer {- ^ time to press button -}
+solve discs = chinese [toMod (t-p-i) n | (i, n, t, p) <- discs] 
