ed6c0ff028ab01c244be277963d2c5c1a386405b
Eric Mertens <emertens@gmail.com>
2022-01-09 16:10:27 -0800

    21

--- solutions/src/2021/21.hs
+++ solutions/src/2021/21.hs
@@ -24,64 +24,95 @@ import Control.Monad (replicateM)
 import Data.List (unfoldr)
 import Data.Map (Map)
 import Data.Map.Strict qualified as Map
-
+import Debug.Trace
 -- | >>> :main
 -- 428736
 -- 57328067654557
 main :: IO ()
 main =
  do (p1,p2) <- [format|2021 21 Player 1 starting position: %u%nPlayer 2 starting position: %u%n|]
-    print (part1 0 p1 p2 0 0)
+    print (part1 p1 p2)
     print (part2 p1 p2)
+
+-- * Part 1
 
 -- | Compute the @die rolls * losing score@ once one player
 -- wins with 1000 points.
+--
+-- >>> part1 4 8
+-- 739785
 part1 ::
+  Int ->
+  Int ->
+  Int
+part1 p1 p2 = p1step 0 p1 p2 0 0
+
+-- | Worker for 'part1'
+p1step ::
   Int {- ^ turn counter -} ->
   Int {- ^ player 1 location -} ->
   Int {- ^ player 2 location -} ->
   Int {- ^ player 1 score    -} ->
   Int {- ^ player 2 score    -} ->
   Int {- ^ player 2 score * 3 * turns -}
-part1 turns p1 p2 p1s p2s
+p1step turns p1 p2 p1s p2s
   | p1s' >= 1000 = 3 * turns' * p2s
-  | otherwise    = part1 turns' p2 p1' p2s p1s'
+  | otherwise    = p1step turns' p2 p1' p2s p1s'
   where
     turns' = turns + 1
     p1'    = wrap (p1 + 6 - turns) 10
     p1s'   = p1s + p1'
 
+-- * Part 2
+
 -- | Count the number of ways player 1 can win given that the game is played
 -- rolling 3d3.
+--
+-- >>> part2 4 8
+-- 444356092776315
 part2 ::
   Int {- ^ player 1's starting location -} ->
   Int {- ^ player 2's starting location -} ->
   Int {- ^ ways player 1 can win -}
-part2 p1 p2 = sum (zipWith (*) p1Wins p2Live)
-  where
-    rolls  = counts (sum <$> replicateM 3 [1..3])
-    p1Wins = unfoldr (p2step 21 rolls) (Map.singleton (p1,0) 1)
-    p2Wins = unfoldr (p2step 21 rolls) (Map.singleton (p2,0) 1)
-    p2Live = scanl (\acc w -> acc * sum rolls - w) 1 p2Wins
+part2 p1 p2 = sum (zipWith (*) (wins p1) (loses p2))
 
--- | Play a turn of the part 2 game.
+-- | Compute the ways a player can win in part 2 per turn given a starting position.
+--
+-- >>> wins 4
+-- [0,0,4608,249542,3219454,24905476,77993473,62172638,8678745,53217]
+wins :: Int -> [Int]
+wins x = unfoldr p2step (Map.singleton (x,0) 1)
+
+-- | Compute the ways a player can not win in part 2 per turn given a starting position.
+--
+-- >>> loses 8
+-- [1,27,729,17953,254050,1411009,3520415,2121762,219716,1206,0]
+loses :: Int -> [Int]
+loses =  scanl (\acc w -> acc * sum rolls - w) 1 . wins
+
+-- | Worker for 'part2'
 p2step ::
-  Int                    {- ^ target score -} ->
-  Map Int Int            {- ^ roll distribution -} ->
   Map.Map (Int, Int) Int {- ^ live games ((location, score), ways) -} ->
   Maybe (Int, Map.Map (Int, Int) Int) {- ^ wins and next turn's live games -}
-p2step goal rolls games
+p2step games
   | Map.null games = Nothing
   | otherwise      = Just (sum wins, live)
   where
     (wins, live) =
-      Map.partitionWithKey (\(_,score) _ -> score >= goal) $
+      Map.partitionWithKey (\(_,score) _ -> score >= 21) $
       Map.fromListWith (+)
         [ ((loc', score + loc'), n1*n2)
           | ((loc, score),n1) <- Map.toList games
           , (roll        ,n2) <- Map.toList rolls
           , let loc' = wrap (loc+roll) 10
           ]
+
+-- | Outcomes of rolling 3d3
+--
+-- >>> rolls
+-- fromList [(3,1),(4,3),(5,6),(6,7),(7,6),(8,3),(9,1)]
+rolls :: Map Int Int
+rolls  = counts (sum <$> replicateM 3 [1..3])
 
 -- * Modular arithmetic
 
