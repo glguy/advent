461472ed00842fdbcbc7e52f520c9e141394e600
Eric Mertens <emertens@galois.com>
2022-01-11 11:03:50 -0800

    9

--- solutions/src/2021/09.hs
+++ solutions/src/2021/09.hs
@@ -9,52 +9,75 @@ Maintainer  : emertens@gmail.com
 
 Find the basins on the height map.
 
+>>> :{
+let exampleGrid =
+      Data.Array.Unboxed.listArray
+        (C 0 0, C 4 9)
+        [2,1,9,9,9,4,3,2,1,0,
+        3,9,8,7,8,9,4,9,2,1,
+        9,8,5,6,7,8,9,8,9,2,
+        8,7,6,7,8,9,6,7,8,9,
+        9,8,9,9,9,6,5,6,7,8]
+:}
+
+>>> let exampleBasins = toBasinIds exampleGrid
+
+>>> basinRiskSum exampleGrid exampleBasins
+15
+
+>>> basinSizes exampleBasins
+[3,9,14,9]
+
 -}
 module Main (main) where
 
 import Advent (counts, getInputArray, arrIx)
 import Advent.Coord (Coord(..), cardinal)
-import Data.Array.Unboxed (Array, UArray, (!), array, assocs, bounds, elems)
+import Data.Array.Unboxed (Array, UArray, (!), amap, array, assocs, bounds, elems)
 import Data.Char (digitToInt)
 import Data.Foldable (toList)
 import Data.List (sortBy)
 import Data.Maybe (catMaybes)
 
-type Heights  = Array Coord (Maybe Int)
-type BasinIds = Array Coord (Maybe Coord)
-
--- >>> :main
+-- | >>> :main
 -- 588
 -- 964712
 main :: IO ()
 main =
- do heights <- heightArray <$> getInputArray 2021 9
-    let basinIds = heightsToBasinIds heights
-
-    print (sum [1+h | (c, Just h) <- assocs heights, Just c == basinIds!c])
+ do heights <- amap digitToInt <$> getInputArray 2021 9
+    let basinIds = toBasinIds heights
+    print (basinRiskSum heights basinIds)
     print (product (top 3 (basinSizes basinIds)))
 
--- | Returns the @n@ largest elements of a list
-top :: Ord a => Int -> [a] -> [a]
-top n = take n . sortBy (flip compare)
+-- * Basins
 
--- | Convert that ASCII grid into a grid of heights
-heightArray :: UArray Coord Char -> Heights
-heightArray a = array (bounds a) [(c, cvt x) | (c, x) <- assocs a]
-  where
-    cvt '9' = Nothing -- 9s are defined to be out of bounds in the problem
-    cvt x   = Just (digitToInt x)
+-- | Array of locations in the grid and the unique low point they flow towards.
+type BasinIds = Array Coord (Maybe Coord)
 
-heightsToBasinIds :: Heights -> BasinIds
-heightsToBasinIds heights = basinIds
+-- | Compute the basin low points for each location on the grid.
+toBasinIds :: UArray Coord Int -> BasinIds
+toBasinIds heights = basinIds
   where
-    basinIds = array (bounds heights) [(c, basinId c =<< mbh) | (c, mbh) <- assocs heights]
+    basinIds = array (bounds heights) [(c, basinId c h) | (c, h) <- assocs heights]
+    
+    basinId _ 9 = Nothing -- problem defines height 9 not to be in a basin
     basinId c h =
-     do xs <- sequence [basinIds!x | x <- cardinal c, Just (Just xh) <- [arrIx heights x], xh<h]
+     do xs <- sequence [basinIds!x | x <- cardinal c, Just xh <- [arrIx heights x], xh<h]
         case xs of
           []                  -> Just c -- this is the lowest point in the basin
           y:ys | all (y==) ys -> Just y -- all flows go to a unique basin
                | otherwise    -> Nothing -- no unique basin
 
+-- | Compute the sum of the risk values of the basin low points.
+basinRiskSum :: UArray Coord Int -> BasinIds -> Int
+basinRiskSum heights basinIds = sum [1+h | (c, h) <- assocs heights, Just c == basinIds!c]
+
+-- | List of the sizes of all the basins on the grid.
 basinSizes :: BasinIds -> [Int]
 basinSizes = toList . counts . catMaybes . elems
+
+-- * List utilities
+
+-- | Returns the @n@ largest elements of a list
+top :: Ord a => Int -> [a] -> [a]
+top n = take n . sortBy (flip compare)
