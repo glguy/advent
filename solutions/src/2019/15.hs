2c09c21162c1a47c4019729cc173f8a535372f31
Eric Mertens <emertens@galois.com>
2022-01-12 14:43:04 -0800

    15

--- solutions/src/2019/15.hs
+++ solutions/src/2019/15.hs
@@ -15,7 +15,6 @@ import Advent.Format (format)
 import Advent.Coord (Coord, north, south, east, west, origin, drawCoords)
 import Advent.Search (bfsOn)
 import Intcode (Effect(..), run, new)
-import Data.Tree (Tree(Node, rootLabel, subForest))
 
 -- | >>> :main
 -- █████████████ ███████████ ███████ █████
@@ -62,36 +61,34 @@ import Data.Tree (Tree(Node, rootLabel, subForest))
 main :: IO ()
 main =
  do intcode <- [format|2019 15 %d&,%n|]
-    let part1:_ = filter (onOxygen . rootLabel)
-                    (explore (searchTree (run (new intcode))))
-        d1   = distance (rootLabel part1)
-        maze = map rootLabel (explore part1)
-        d2   = distance (last maze)
-    putStr (drawCoords (map location maze))
-    print d1
-    print (d2 - d1)
+    let part1:_ = filter onOxygen
+                    (explore (initialState (run (new intcode))))
+        part2 = explore part1{distance = 0}
+    putStr (drawCoords (map location part2))
+    print (distance part1)
+    print (distance (last part2))
 
 data SearchState = SearchState
   { onOxygen :: !Bool  -- ^ Is the robot currently on the oxygen?
   , distance :: !Int   -- ^ Commands issued so far
   , location :: !Coord -- ^ robot's current location
+  , effect   :: Effect -- ^ current program state
   }
 
--- | Breadth-first search of the program execution space.
-explore :: Tree SearchState -> [Tree SearchState]
-explore = bfsOn (location . rootLabel) subForest
+-- | Build a new search state for a program effect. Assume we're at the
+-- origin and that we are not on oxygen.
+initialState :: Effect -> SearchState
+initialState = SearchState False 0 origin
 
--- | Produce the tree of all possible executions of the program.
--- Each tree child will be the result of one of the possible
--- movements at that point in the program.
-searchTree :: Effect -> Tree SearchState
-searchTree = search (SearchState False 0 origin)
+-- | Breadth-first search of the program execution visiting each
+-- location in the maze once.
+explore :: SearchState -> [SearchState]
+explore = bfsOn location step
 
--- | Worker loop for 'searchTree'
-search :: SearchState -> Effect -> Tree SearchState
-search s@(SearchState oxy dist loc) (Input f) =
-  Node s
-    [ search (SearchState (o == 2) (dist + 1) (loc + dir)) e'
+-- | Advance the program forward one input/output step.
+step :: SearchState -> [SearchState]
+step (SearchState oxy dist loc (Input f)) =
+  [ SearchState (o == 2) (dist + 1) (loc + dir) e'
     | (i, dir) <- [(1,north),(2,south),(3,west),(4,east)]
     , let Output o e' = f i
     , o > 0]
