41fba98c944e2f1caa35bd24e06a83f16c798d61
Eric Mertens <emertens@galois.com>
2022-01-12 14:14:46 -0800

    13

--- solutions/src/2019/13.hs
+++ solutions/src/2019/13.hs
@@ -1,64 +1,102 @@
-{-# Language BlockArguments, QuasiQuotes, ImportQualifiedPost #-}
+{-# Language BlockArguments, LambdaCase, QuasiQuotes, ImportQualifiedPost #-}
 {-|
 Module      : Main
 Description : Day 13 solution
-Copyright   : (c) Eric Mertens, 2019
+Copyright   : (c) Eric Mertens, 2021
 License     : ISC
 Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2019/day/13>
 
+Play a game of breakout! This solution uses a greedy strategy of
+simply moving the paddle toward the ball and waiting for a win.
+
 -}
 module Main (main) where
 
-import Advent.Format (format)
+import Advent (count, format)
+import Advent.Coord (Coord(C), drawPicture)
 import Data.List (foldl')
-import Data.Set qualified as Set
-import Intcode
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Intcode (Effect(Input, Halt, Output), Machine, run, new, set)
 
 -- | >>> :main
+-- █████████████████████████████████████████████
+-- █                                           █
+-- █   ◇◇ ◇◇◇◇◇ ◇  ◇ ◇◇◇ ◇◇◇◇ ◇◇◇◇ ◇◇◇ ◇  ◇◇◇  █
+-- █ ◇ ◇◇◇◇  ◇◇  ◇◇ ◇◇◇◇ ◇◇◇◇   ◇◇◇ ◇ ◇◇◇◇◇◇◇  █
+-- █   ◇◇◇◇   ◇  ◇◇◇◇◇◇◇◇  ◇   ◇◇◇◇◇ ◇◇  ◇◇ ◇◇ █
+-- █ ◇◇ ◇◇   ◇ ◇◇ ◇ ◇◇◇◇◇  ◇◇◇ ◇  ◇◇ ◇ ◇ ◇◇◇◇◇ █
+-- █ ◇  ◇◇◇  ◇◇  ◇◇◇◇◇◇ ◇◇◇◇◇ ◇◇ ◇  ◇◇◇◇◇◇◇  ◇ █
+-- █ ◇ ◇◇◇◇◇◇  ◇ ◇◇◇◇ ◇◇◇◇◇◇◇ ◇◇◇ ◇◇◇ ◇ ◇◇◇ ◇◇ █
+-- █ ◇◇◇◇◇◇ ◇◇◇◇  ◇ ◇ ◇  ◇◇ ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ █
+-- █ ◇ ◇◇◇◇◇◇ ◇◇ ◇◇◇◇◇◇◇   ◇◇  ◇◇◇◇ ◇ ◇◇◇◇◇◇◇◇ █
+-- █ ◇◇◇◇◇◇◇◇◇◇◇ ◇◇◇◇◇◇◇ ◇ ◇ ◇ ◇◇◇◇  ◇◇◇◇ ◇◇◇◇ █
+-- █ ◇◇ ◇◇  ◇◇◇◇◇◇◇◇◇ ◇◇ ◇ ◇◇ ◇◇◇◇◇◇  ◇◇◇◇◇ ◇  █
+-- █  ◇◇ ◇ ◇◇◇◇◇   ◇◇◇  ◇◇◇◇◇◇ ◇◇◇ ◇◇  ◇◇◇◇ ◇  █
+-- █  ◇◇◇ ◇◇ ◇◇◇ ◇◇◇◇  ◇◇◇◇◇◇◇◇◇ ◇◇ ◇◇ ◇◇◇ ◇◇◇ █
+-- █ ◇◇◇ ◇◇◇◇◇◇◇ ◇ ◇ ◇◇◇◇◇◇◇    ◇◇◇◇  ◇◇◇◇◇◇◇◇ █
+-- █ ◇◇    ◇◇◇◇◇ ◇◇◇   ◇◇ ◇◇   ◇◇◇ ◇◇◇◇◇◇◇◇◇◇◇ █
+-- █  ◇◇◇  ◇◇ ◇ ◇◇◇  ◇◇ ◇   ◇  ◇ ◇◇◇  ◇◇ ◇◇◇◇◇ █
+-- █  ◇◇◇  ◇◇   ◇ ◇◇ ◇ ◇◇◇◇◇◇◇◇◇◇ ◇◇ ◇◇◇ ◇◇◇◇◇ █
+-- █                                           █
+-- █                   ✪                       █
+-- █                                           █
+-- █                                           █
+-- █                     ―                     █
+-- █                                           █
 -- 462
 -- 23981
 main :: IO ()
 main =
-  do mach <- new <$> [format|2019 13 %d&,%n|]
+ do mach <- new <$> [format|2019 13 %d&,%n|]
+    let picture1 = part1 (run mach)
+    putStr (drawPicture (fmap paint picture1))
+    print (count 2 picture1)
+    print (part2 Nothing Nothing 0 (run (set 0 2 mach)))
 
-     print (part1 mach)
-     print (robot Nothing Nothing 0 (run (set 0 2 mach)))
-
-part1 :: Machine -> Int
-part1 = Set.size . foldl' write Set.empty . tileWrites . run
+-- | Count the number of screen locations assigned to @2@.
+part1 :: Effect -> Map Coord Int
+part1 = go Map.empty
   where
-    write blocks (x,y,2) = Set.insert (x,y) blocks
-    write blocks (x,y,_) = Set.delete (x,y) blocks
+    go blocks = \case
+      Halt -> blocks
+      Output x (Output y (Output t e)) ->
+        go (Map.insert (C y x) t blocks) e
+      _ -> error "part1: bad program"
 
-tileWrites :: Effect -> [(Int, Int, Int)]
-tileWrites effect =
-  case effect of
-    Halt                                   -> []
-    Output x (Output y (Output t effect')) -> (x,y,t) : tileWrites effect'
-    _                                      -> error "tileWrites: bad program"
+-- | Play the breakout game to completion and report the final score.
+part2 ::
+  Maybe Int {- ^ location of ball -} ->
+  Maybe Int {- ^ location of paddle -} ->
+  Int       {- ^ current score -} ->
+  Effect    {- ^ program effect -} ->
+  Int       {- ^ final score -}
+part2 ball paddle score = \case
+  Halt -> score
 
-robot :: Maybe Int -> Maybe Int -> Int -> Effect -> Int
-robot ball paddle score effect =
-  case effect of
+  Output (-1) (Output 0 (Output score' effect')) ->
+    part2 ball paddle score' effect'
 
-    Halt -> score
+  Output x (Output _ (Output t effect'))
+    | t == 3 -> part2 ball (Just x) score effect'
+    | t == 4 -> part2 (Just x) paddle score effect'
+    | otherwise -> part2 ball paddle score effect'
 
-    Output (-1) (Output 0 (Output score' effect')) ->
-      robot ball paddle score' effect'
+  Input f ->
+    part2 ball paddle score
+      case (ball, paddle) of
+        (Just b, Just p) -> f (signum (b-p))
+        _ -> f 0
 
-    Output x (Output _ (Output t effect'))
-      | t == 3 -> robot ball (Just x) score effect'
-      | t == 4 -> robot (Just x) paddle score effect'
-      | otherwise -> robot ball paddle score effect'
+  _ -> error "part2: bad program"
 
-    Input f ->
-        robot ball paddle score
-         case (ball, paddle) of
-           (Just b, Just p)
-             | b < p -> f (-1)
-             | b > p -> f 1
-           _ -> f 0
-
-    _ -> error "robot: bad program"
+-- | Render tiles as characters for viewing.
+paint :: Int -> Char
+paint 0 = ' '
+paint 1 = '█'
+paint 2 = '◇'
+paint 3 = '―'
+paint 4 = '✪'
+paint _ = error "paint: invalid argument"
