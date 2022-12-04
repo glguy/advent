edcd10d3ff8ac3711ad2739426e928e9d92b7157
Eric Mertens <emertens@gmail.com>
2022-12-03 22:12:06 -0800

    more reuse

--- solutions/src/2022/04.hs
+++ solutions/src/2022/04.hs
@@ -20,11 +20,8 @@ import Advent ( format, countBy )
 main :: IO ()
 main =
  do input <- [format|2022 4 (%u-%u,%u-%u%n)*|]
-    print $ countBy (\(a,b,c,d) -> subset (a,b) (c,d) || subset (c,d) (a,b)) input
+    print $ countBy (\(a,b,c,d) -> inRange (a,b) c && inRange (a,b) d || inRange (c,d) a && inRange (c,d) b) input
     print $ countBy (\(a,b,c,d) -> inRange (a,b) c || inRange (a,b) d || inRange (c,d) a || inRange (c,d) b) input
-
-subset :: Ord a => (a, a) -> (a, a) -> Bool
-subset (a,b) (c,d) = c <= a && b <= d
 
 inRange :: Ord a => (a, a) -> a -> Bool
 inRange (a,b) x = a <= x && x <= b
