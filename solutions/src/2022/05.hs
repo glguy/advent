8ad39d39e2635e2e9a57100a66c242bfb78c0172
Eric Mertens <emertens@gmail.com>
2022-12-04 21:40:31 -0800

    more robust parser 5

--- solutions/src/2022/05.hs
+++ solutions/src/2022/05.hs
@@ -12,6 +12,7 @@ Maintainer  : emertens@gmail.com
 module Main where
 
 import Data.List (transpose)
+import Data.Maybe (catMaybes)
 import Data.Map (Map)
 import Data.Map qualified as Map
 
@@ -19,8 +20,12 @@ import Advent (format)
 
 main :: IO ()
 main = do
-    (toppart, commands) <- [format|2022 5 (%c+%n)*%n(move %u from %c to %c%n)*|]
-    let stacks = parseStacks toppart
+    (toppart, labels, commands) <- [format|2022 5
+        ((   |[%c])& %n)*
+        ( %c )& %n
+        %n
+        (move %u from %c to %c%n)*|]
+    let stacks = Map.fromList (zip labels (map catMaybes (transpose toppart)))
     let solve f = map head (Map.elems (foldl (apply f) stacks commands))
     putStrLn (solve reverse)
     putStrLn (solve id     )
@@ -29,10 +34,3 @@ apply :: Ord k => ([a] -> [a]) -> Map k [a] -> (Int, k, k) -> Map k [a]
 apply f stacks (n, fr, to) =
     case Map.alterF (traverse (splitAt n)) fr stacks of
         (a, m) -> Map.adjust (f a ++) to m
-
-parseStacks :: [String] -> Map Char String
-parseStacks strs =
-    Map.fromList
-        [(n, dropWhile (' '==) (reverse row))
-            | n:row <- map reverse (transpose strs)
-            , n /= ' ']
