8a781dfe7423a0b481cd7cfcda12d3726d67543c
Eric Mertens <emertens@gmail.com>
2022-12-06 21:40:45 -0800

    comments

--- solutions/src/2022/07.hs
+++ solutions/src/2022/07.hs
@@ -16,6 +16,9 @@ import Data.List (inits)
 
 import Advent (format)
 
+-- | File paths are represented as a list of directory components
+type Path = [String]
+
 main :: IO ()
 main =
  do input <- [format|2022 7
@@ -23,10 +26,10 @@ main =
       |$ ls%n
       (%u %s%n
       |dir %s%n)*)*|]
-    
+
     let allFiles = simulate [] input
     let dirTree = Map.fromListWith (+) [(d',n) | (d,n) <- allFiles, d' <- inits d]
-    
+
     -- part 1
     print (sum [n | n <- Map.elems dirTree, n <= 100000])
 
@@ -34,10 +37,15 @@ main =
     let totalUsed = sum [n | (_,n) <- allFiles]
     print $ minimum [freed | (_,freed) <- Map.assocs dirTree,
                              70000000 - totalUsed + freed >= 30000000 ]
-    
-simulate :: [String] -> [Either String [Either (Int, String) String]] -> [([String], Int)]
+
+-- | Given a list of cd commands and directory lists, generate a list
+-- of files contained alongside the file sizes.
+simulate ::
+  Path {- ^ current working directory components -} ->
+  [Either String [Either (Int, String) String]] {- ^ list of either a cd or a directory listing -} ->
+  [(Path, Int)] {- ^ list of files and their sizes -}
 simulate _   [] = []
-simulate _ (Left "/"  : xs) = simulate [] xs
-simulate cwd (Left ".." : xs) = simulate (init cwd) xs
-simulate cwd (Left dir  : xs) = simulate (cwd ++ [dir]) xs
+simulate _   (Left "/"   : xs) = simulate [] xs
+simulate cwd (Left ".."  : xs) = simulate (init cwd) xs
+simulate cwd (Left dir   : xs) = simulate (cwd ++ [dir]) xs
 simulate cwd (Right list : xs) = (cwd, sum [n | Left (n,_) <- list]) : simulate cwd xs
