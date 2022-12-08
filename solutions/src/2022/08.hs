b298bc7e8916dc6bb87316e6a537877716019e45
Eric Mertens <emertens@gmail.com>
2022-12-07 21:24:52 -0800

    2022-08

---
+++ solutions/src/2022/08.hs
@@ -0,0 +1,53 @@
+{-# Language QuasiQuotes, ImportQualifiedPost #-}
+{-|
+Module      : Main
+Description : Day 8 solution
+Copyright   : (c) Eric Mertens, 2022
+License     : ISC
+Maintainer  : emertens@gmail.com
+
+<https://adventofcode.com/2022/day/8>
+
+-}
+module Main where
+
+import Data.Array.Unboxed (Ix(inRange, range), UArray, IArray(..), (!))
+import Advent (getInputArray, countBy)
+import Advent.Coord (above, below, left, right, Coord)
+
+main :: IO ()
+main =
+ do input <- getInputArray 2022 08
+    print (countBy (isEdgeVisible input) (range (bounds input)))
+    print (maximum (map (scenicScore input) (range (bounds input))))
+
+-- | Return the list of elements in the array starting 
+outToEdge ::
+    UArray Coord Char {- ^ array -} ->
+    Coord             {- ^ starting coordinate -} ->
+    (Coord -> Coord)  {- ^ coordinate step function -} ->
+    [Char]            {- ^ list of elements out to the edge of the array -}
+outToEdge a c dir = [a ! i | i <- takeWhile (inRange (bounds a)) (iterate dir c)]
+
+sightLines ::
+    UArray Coord Char {- ^ array -} ->
+    Coord             {- ^ starting coordinate -} ->
+    [[Char]]          {- ^ list of trees viewed in each cardinal direction -}
+sightLines a c = map (outToEdge a c) [above,below,left,right]
+
+isEdgeVisible :: UArray Coord Char -> Coord -> Bool
+isEdgeVisible a c = any clearView (sightLines a c)
+
+clearView :: [Char] -> Bool
+clearView [] = error "clearView: empty list"
+clearView (x:xs) = all (<x) xs
+
+scenicScore :: UArray Coord Char -> Coord -> Int
+scenicScore a c = product (map treesSeen (sightLines a c))
+
+treesSeen :: [Char] -> Int
+treesSeen [] = error "cansee: empty list"
+treesSeen (x:xs) =
+    case break (>= x) xs of
+        (a,[])  -> length a
+        (a,_:_) -> length a + 1
