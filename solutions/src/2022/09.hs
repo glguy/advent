a043dd3806d51e8f856b7011b4fe7ac5969ea1ea
Eric Mertens <emertens@galois.com>
2022-12-09 09:18:11 -0800

    comment all of 9

--- solutions/src/2022/09.hs
+++ solutions/src/2022/09.hs
@@ -11,15 +11,23 @@ Maintainer  : emertens@gmail.com
 -}
 module Main where
 
-import Advent ( format, ordNub, stageTH )
-import Advent.Coord ( above, below, left, origin, right, Coord(C) )
+import Advent (format, stageTH)
+import Advent.Coord (Coord(..), origin, east, north, south, west)
 import Data.Set qualified as Set
 
-data C = CD | CR | CU | CL deriving Show
+-- | Rope movement instructions
+data C
+  = CD -- ^ move down
+  | CR -- ^ move right
+  | CU -- ^ move up
+  | CL -- ^ move left
+  deriving Show
 
 stageTH
 
--- |
+-- | Print the answers to both parts of day 9. Automatically finds
+-- input file unless overridden with a command line argument.
+--
 -- >>> :main
 -- 5930
 -- 2443
@@ -31,25 +39,44 @@ main = do
     print (length (Set.fromList (knots !! 9)))
 
 -- | Generate a stream of lists, one for each knot in the rope.
+-- Each list contains the coordinates of that knot at each time step.
 infiniteRope :: [C] -> [[Coord]]
-infiniteRope = iterate (scanl1 updateTail) . scanl drive origin
+infiniteRope
+  = iterate (scanl1 updateTail)  -- repeatedly apply tail update operation
+  . scanl (+) origin             -- compute partial sums of steps
+  . map cToVec                   -- convert input commands to step vectors
 
+-- | Replicate the first element second element number of times.
+--
+-- >>> expand ('a', 5)
+-- "aaaaa"
 expand :: (a, Int) -> [a]
 expand (x,n) = replicate n x
 
-drive :: Coord -> C -> Coord
-drive here CD = below here
-drive here CU = above here
-drive here CL = left  here
-drive here CR = right here
+-- | Generate the unit vector corresponding to an input command.
+cToVec :: C -> Coord
+cToVec CU = north
+cToVec CD = south
+cToVec CR = east
+cToVec CL = west
 
+-- | Compute the next tail position given an updated head position.
 updateTail ::
-  Coord {- ^ tail -} ->
-  Coord {- ^ head -} ->
-  Coord
-updateTail t@(C ty tx) h@(C hy hx)
-  | touching ty hy, touching tx hx = t
-  | otherwise = t + signum (h-t)
+  Coord {- ^ old tail -} ->
+  Coord {- ^ new head -} ->
+  Coord {- ^ new tail -} 
+updateTail tailKnot headKnot
+  | isNearOrigin delta = tailKnot
+  | otherwise          = tailKnot + signum delta
+  where
+    delta = headKnot - tailKnot
 
-touching :: Int -> Int -> Bool
-touching x y = abs (x - y) < 2
+-- | Predicate for coordinates at or adjacent to the origin.
+--
+-- >>> all isNearOrigin [C y x | y <- [-1..1], x <- [-1..1]]
+-- True
+--
+-- >>> any isNearOrigin [C 2 0, C 0 2, C 2 1, C (-2) 0, C (-1) 2]
+-- False
+isNearOrigin :: Coord -> Bool
+isNearOrigin (C y x) = abs x < 2 && abs y < 2
