1cbcc3337cf7de580d4766260d00860456819b86
Eric Mertens <emertens@gmail.com>
2022-12-11 10:14:00 -0800

    update 11

--- solutions/src/2022/11.hs
+++ solutions/src/2022/11.hs
@@ -1,4 +1,4 @@
-{-# Language QuasiQuotes, ImportQualifiedPost #-}
+{-# Language QuasiQuotes, ImportQualifiedPost, NumericUnderscores #-}
 {-|
 Module      : Main
 Description : Day 11 solution
@@ -8,69 +8,90 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2022/day/11>
 
+This solution relies on the fact that there are no interactions between
+items, so each item can be simulated separately. When a monkey throws an
+item to a monkey with a larger ID that monkey will get to throw the item
+in the same round, but when it throws it to an earlier monkey, that item
+will not be thrown until the next round. Using this we can keep track of
+when an item would stop moving.
+
 -}
 module Main where
 
-import Data.Map (Map)
-import Data.Map qualified as Map
-import Data.List ( sort )
+import Data.Array.Unboxed ( Array, (!), array )
+import Data.Foldable (toList)
+import Data.List (sortBy)
 
-import Advent ( format, times )
-import Advent.Queue qualified as Queue
-import Advent.Queue (Queue)
+import Advent (format, counts)
 
-data Monkey = Monkey {
-  items :: Queue Int,
-  fun :: Int -> Int,
-  divisor :: Int,
-  onTrue :: Int,
-  onFalse :: Int,
-  throws :: Int
-}
+-- | Input file contains a list of:
+--
+-- * Monkey ID
+-- * Starting items
+-- * @+@ or @*@
+-- * A literal or the @old@ variable
+-- * A divisor
+-- * The monkey ID when the divisor divides the value
+-- * The monkey ID when the divisor does not divide the value
+type Input = [(Int, [Int], Char, Maybe Int, Int, Int, Int)]
 
 main :: IO ()
-main = do
-    input <- [format|2022 11
+main =
+ do input <- [format|2022 11
       (Monkey %u:%n
         Starting items: %u&(, )%n
         Operation: new = old %c (old|%u)%n
         Test: divisible by %u%n
           If true: throw to monkey %u%n
           If false: throw to monkey %u%n)&%n|]
-
-    let monkeys = Map.fromList
-          [(i, Monkey (Queue.fromList start) (eval op arg) d t f 0)
-          | (i, start, op, arg, d, t, f) <- input]
     
+    -- It's safe to work with integers mod the lcm of all the divisor tests.
+    -- This will keep the worry numbers small while preserving all the
+    -- divisibility tests.
+    let modulus = foldl1 lcm [d | (_, _, _, _, d, _, _) <- input]
 
-    print $ product $ take 2 $ reverse $ sort $ Map.elems $ fmap throws $ times 20 (runRound Nothing) monkeys
-    let magic = foldl1 lcm [d | (_, _, _, _, d, _, _) <- input]
-    print $ product $ take 2 $ reverse $ sort $ Map.elems $ fmap throws $ times 10000 (runRound (Just magic)) monkeys
+    print (solve (`div`      3)     20 input)
+    print (solve (`mod`modulus) 10_000 input)
 
+-- | Run the given number of monkey throwing rounds and compute monkey business.
+solve ::
+  (Int -> Int) {- ^ extra operation to run after each monkey updates worry -} ->
+  Int          {- ^ number of rounds to run -} ->
+  Input        {- ^ input file contents -} ->
+  Int          {- ^ product of top 2 counts of times each monkey threw something -}
+solve post rounds input = product . top 2 . toList . counts $
+  [ thrower
+    | (i, startingItems, _, _, _, _, _) <- input
+    , item <- startingItems
+    , thrower <- go rounds i item
+  ]
+  where
+    -- list of monkey ids used to find bounds for monkey array
+    ids = [i | (i, _, _, _, _, _, _) <- input]
+
+    -- store monkey information in array for faster random access
+    monkeys :: Array Int (Int -> Int, Int, Int, Int)
+    monkeys = array (minimum ids, maximum ids) [(i, (eval o n,d,t,f)) | (i, _, o, n, d, t, f) <- input]
+
+    go :: Int -> Int -> Int -> [Int]
+    go 0 _ _ = []
+    go r i x =
+      i :
+      case monkeys ! i of
+        (o,d,t,f) -> go r' i' x'
+          where
+            x' = post (o x)
+            i' = if x' `mod` d == 0 then t else f
+            r' = if i' < i then r-1 else r
+
+-- | Evaluate a monkey's worry update function.
 eval :: Char -> Maybe Int -> Int -> Int
-eval '*' (Just n) x  = x * n
-eval '+' (Just n) x  = x + n
-eval '*' Nothing  x = x * x
-eval '+' Nothing  x = x + x
-eval op _ _ = error ("Unexpected operation: " ++ [op])
+eval '*' (Just n) = (n*)
+eval '+' (Just n) = (n+)
+eval '*' Nothing  = (^(2::Int))
+eval '+' Nothing  = (2*)
+eval op _ = error ("Unexpected operation: " ++ [op])
 
-runRound :: Maybe Int -> Map Int Monkey -> Map Int Monkey
-runRound magic ms =
-  foldl (runMonkey magic) ms (Map.keys ms)
-
-runMonkey :: Maybe Int -> Map Int Monkey -> Int ->  Map Int Monkey
-runMonkey magic ms i =
-  let m = ms Map.! i in
-  case items m of
-    Queue.Empty -> ms
-    item Queue.:<| items' ->
-      let val = case magic of Just modulus -> fun m item `mod` modulus
-                              Nothing ->  fun m item `div` 3
-          target = if 0 == val `mod` fromIntegral (divisor m) then onTrue m else onFalse m
-      in
-      runMonkey magic
-      (Map.adjust (push val) target $
-      Map.adjust (\x -> x{items = items', throws=throws m + 1}) i ms) i
-
-push :: Int -> Monkey -> Monkey
-push x m = m { items = Queue.snoc x (items m)}
+-- | Returns the largest @n@ numbers in a list
+top :: Ord a => Int -> [a] -> [a]
+top n = take n . sortBy (flip compare)
