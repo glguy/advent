e0ee70bdc16b83df6ceba78b7a6985f507f8e275
Eric Mertens <emertens@galois.com>
2022-12-14 09:34:40 -0800

    shorter

--- solutions/src/2022/14.hs
+++ solutions/src/2022/14.hs
@@ -26,7 +26,7 @@ import Advent.Coord (below, coordRow, left, right, Coord(..))
 main :: IO ()
 main = do
     input <- [format|2022 14 ((%u,%u)&( -> )%n)*|]
-    let world = Set.fromList [x | xs <- input, x <- segs (map toCoord xs)]
+    let world = Set.fromList (concatMap segs input)
         limit = 1 + maximum [ y| C y _ <- Set.toList world]
 
     case fillFrom1 limit world top of
@@ -42,7 +42,7 @@ top = C 0 500
 fillFrom1 :: Int -> Set Coord -> Coord -> Either (Set Coord) (Set Coord)
 fillFrom1 limit world here
   | limit < coordRow here = Left world
-  | Set.member here world  = Right world
+  | Set.member here world = Right world
   | otherwise = Set.insert here <$> foldM (fillFrom1 limit) world
                   [below here, left (below here), right (below here)]
 
@@ -54,16 +54,13 @@ fillFrom2 limit world here
 
 -- Turning line segments into sets of coordinates
 
-toCoord :: (Int,Int) -> Coord
-toCoord (x,y) = C y x
-
-segs :: [Coord] -> [Coord]
+segs :: [(Int,Int)] -> [Coord]
 segs (x:y:z) = seg x y ++ segs (y:z)
-segs [x] = [x]
-segs [ ] = [ ]
+segs [(x,y)] = [C y x]
+segs []      = []
 
-seg :: Coord -> Coord -> [Coord]
-seg (C a b) (C c d)
-  | a == c    = [C a x | x <- [min b d .. max b d]]
-  | b == d    = [C x d | x <- [min a c .. max a c]]
+seg :: (Int,Int) -> (Int,Int) -> [Coord]
+seg (a,b) (c,d)
+  | a == c    = [C y a | y <- [min b d .. max b d]]
+  | b == d    = [C d x | x <- [min a c .. max a c]]
   | otherwise = error "unexpected input"
