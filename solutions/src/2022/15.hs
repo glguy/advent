0008cb50d12079fe2c53fc644714c7ae5fc295d1
Eric Mertens <emertens@gmail.com>
2022-12-14 23:30:13 -0800

    comments

--- solutions/src/2022/15.hs
+++ solutions/src/2022/15.hs
@@ -23,31 +23,53 @@ type Input = [(Int,Int,Int,Int)]
 -- 4724228
 -- 13622251246513
 main :: IO ()
-main = do
-    input <- [format|2022 15 (Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d%n)*|]
+main =
+ do input <- [format|2022 15 (Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d%n)*|]
+    print (part1 input)
+    print (part2 input)
 
-    let p1y = 2_000_000
-    print $ sum $ map size $
-        removeallof (beaconsAtY input p1y) $
-        boxUnion [y | x <- input, y <- ranges p1y x]
+-- part 1 logic
 
-    print $ head [
-      4_000_000 * y + x
-      | C y x <-
-          map fromdiamond $
-          removeallof (todiamonds input)
-          [todiamond (C 2000000 2000000) 4000000 ]
-      , 0 <= y, y <= 4000000, 0 <= x , x <= 4000000]
+part1 :: Input -> Int
+part1 input =
+  let p1y = 2_000_000 in
+  sum $ map size $
+  removeallof (beaconsAtY input p1y) $
+  makeDisjoint [y | x <- input, y <- ranges p1y x]
 
+beaconsAtY :: Input -> Int -> [Box ('S 'Z)]
+beaconsAtY input ty = [Dim nx (nx+1) Pt | (_,_,nx,ny)<-input, ny == ty]
+
+ranges :: Int -> (Int,Int,Int,Int) -> [Box ('S 'Z)]
+ranges yy (x,y,nx,ny)
+  | dx < 0 = []
+  | otherwise = [cover x dx Pt]
+    where
+        dy = abs (yy - y)
+        dx = dist - dy
+        dist = manhattan (C y x) (C ny nx)
+
+-- part 2 logic
+
+part2 :: Input -> Int
+part2 input = head
+  [ 4_000_000 * y + x
+    | C y x <-
+        map fromdiamond $
+        removeallof (todiamonds input)
+        [todiamond (C 2_000_000 2_000_000) 4_000_000]
+    , 0 <= y, y <= 4_000_000, 0 <= x, x <= 4_000_000]
+
+-- | Find a corner of a diamond represented as a square region.
 fromdiamond :: Box ('S ('S 'Z)) -> Coord
 fromdiamond (Dim xpy _ (Dim xmy _ Pt)) = C ((xpy + xmy) `div` 2) ((xpy - xmy) `div` 2)
 
+-- | Covert a diamond centered at a coordinate with a radius into a square region.
 todiamond :: Coord -> Int -> Box ('S ('S 'Z))
-todiamond (C y x) r =  Dim (a - r) (a + r + 1) (Dim (b - r) (b + r + 1) Pt)
-  where
-    a = x + y
-    b = x - y
+todiamond (C y x) r = cover (x+y) r (cover (x-y) r Pt)
 
+-- | Convert all the sensors in the input file into square regions corresponding to the
+-- diamond covered by the sensor.
 todiamonds :: Input -> [Box ('S ('S 'Z))]
 todiamonds input =
   [ todiamond (C y x) r
@@ -55,26 +77,23 @@ todiamonds input =
      , let r = manhattan (C y x) (C ny nx)
      ]
 
+-- Box utilities
 
-beaconsAtY :: Input -> Int -> [Box ('S 'Z)]
-beaconsAtY input ty = [Dim nx (nx+1) Pt | (_,_,nx,ny)<-input, ny == ty]
-
-ranges :: Int -> (Int,Int,Int,Int) -> [Box ('S 'Z)]
-ranges yy (x,y,nx,ny) 
-  | dx < 0 = []
-  | otherwise = [Dim (x - dx) (x + dx+1) Pt]
-    where
-        dy = abs (yy - y)
-        dx = dist - dy
-        dist = manhattan (C y x) (C ny nx)
-
--- Box helpers
-
-removeallof :: [Box n] -> [Box n] -> [Box n]
+-- | Remove the first list of regions from the second.
+removeallof ::
+  [Box n] {- ^ remove this -} ->
+  [Box n] {- ^ from this -} ->
+  [Box n] {- ^ remaining region -}
 removeallof xs ys = foldl remove1 ys xs
- where remove1 acc x = concatMap (subtractBox x) acc
+  where remove1 acc x = concatMap (subtractBox x) acc
 
-boxUnion :: [Box a] -> [Box a]
-boxUnion = foldr add []
+-- | Extend a box to cover a new dimension centered on x with radius r.
+cover :: Int {- ^ position -} -> Int {- ^ radius -} -> Box n -> Box ('S n)
+cover x r = Dim (x - r) (x + r + 1)
+
+-- | Given a list of potentially overlapping boxes create a new list
+-- of boxes that cover the same region but which do not overlap
+makeDisjoint :: [Box a] -> [Box a]
+makeDisjoint = foldr add []
   where
     add x sofar = x : concatMap (subtractBox x) sofar
