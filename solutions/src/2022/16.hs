a13c441393b3bf8cdd9aaaa0cb2a0d9a3f23da81
Eric Mertens <emertens@galois.com>
2022-12-16 13:41:13 -0800

    16 doctest

--- solutions/src/2022/16.hs
+++ solutions/src/2022/16.hs
@@ -8,6 +8,22 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2022/day/16>
 
+>>> :{
+:main +
+    "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\n\
+    \Valve BB has flow rate=13; tunnels lead to valves CC, AA\n\
+    \Valve CC has flow rate=2; tunnels lead to valves DD, BB\n\
+    \Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE\n\
+    \Valve EE has flow rate=3; tunnels lead to valves FF, DD\n\
+    \Valve FF has flow rate=0; tunnels lead to valves EE, GG\n\
+    \Valve GG has flow rate=0; tunnels lead to valves FF, HH\n\
+    \Valve HH has flow rate=22; tunnel leads to valve GG\n\
+    \Valve II has flow rate=0; tunnels lead to valves AA, JJ\n\
+    \Valve JJ has flow rate=21; tunnel leads to valve II\n"
+:}
+1651
+1707
+
 -}
 module Main where
 
@@ -19,6 +35,10 @@ import Data.List (tails)
 
 import Advent ( format )
 
+-- |
+-- :main
+-- 1820
+-- 2602
 main :: IO ()
 main = do
     input <- [format|2022 16 (Valve %s has flow rate=%u; tunnel(|s) lead(|s) to valve(|s) %s&(, )%n)*|]
