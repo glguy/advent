6f51fa69f0efe9b6952e0e3e74384219cb08d846
Eric Mertens <emertens@gmail.com>
2022-12-15 23:51:47 -0800

    simplify the code

--- solutions/src/2022/16.hs
+++ solutions/src/2022/16.hs
@@ -24,22 +24,21 @@ main = do
     input <- [format|2022 16 (Valve %s has flow rate=%u; tunnel(|s) lead(|s) to valve(|s) %s&(, )%n)*|]
     let m = Map.fromList [(k, (n, vs)) | (k,n,vs) <- input]
 
-    let routeValues1 = Map.fromListWith max [(open, n) | (_, open, n) <- solver m 30]
+    let routeValues1 = solver m 30
     print (maximum routeValues1)
     
-    let routeValues2 = Map.fromListWith max [(open, n) | (_, open, n) <- solver m 26]
+    let routeValues2 = solver m 26
     print (maximum [ v+v1  | (k,v):ys <- tails (Map.assocs routeValues2), (k1,v1) <- ys, Set.null (Set.intersection k k1)])
 
-solver :: Map String (Int, [String]) -> Int -> [(String, Set String, Int)]
-solver m = go [("AA", Set.empty, 0)]
+solver :: Map String (Int, [String]) -> Int -> Map (Set String) Int
+solver m = go [(("AA", Set.empty), 0)]
   where
-    go x 0 = x
-    go x t = go (simplify (concatMap step x)) (t-1)
+    go states 0 = Map.fromListWith max [(open, n) | ((_, open), n) <- states]
+    go states t = go (simplify (concatMap step states)) (t-1)
         where
-            step (here, open, n) =
-                [(next, open, n) | next <- snd (m Map.! here) ] ++
-                [(here, Set.insert here open, n + (t-1) * amt)
+            step ((here, open), n) =
+                [((next, open), n) | next <- snd (m Map.! here)] ++
+                [((here, Set.insert here open), n + (t-1) * amt)
                     | Set.notMember here open
-                    , let amt = (fst $ m Map.! here), amt /= 0 ]
-    simplify xs =
-        [(here,open,n) | ((here,open),n) <- Map.assocs (Map.fromListWith max [((here, open), n) | (here, open, n) <- xs])]
+                    , let amt = fst (m Map.! here), amt /= 0 ]
+    simplify = Map.assocs . Map.fromListWith max
