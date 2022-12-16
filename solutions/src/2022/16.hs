5325db8e8f309ce49187d303dd6be8f3219cfbb1
Eric Mertens <emertens@gmail.com>
2022-12-15 23:54:59 -0800

    fuss

--- solutions/src/2022/16.hs
+++ solutions/src/2022/16.hs
@@ -22,23 +22,25 @@ import Advent ( format )
 main :: IO ()
 main = do
     input <- [format|2022 16 (Valve %s has flow rate=%u; tunnel(|s) lead(|s) to valve(|s) %s&(, )%n)*|]
-    let m = Map.fromList [(k, (n, vs)) | (k,n,vs) <- input]
+    let graph = Map.fromList [(k, (n, vs)) | (k, n, vs) <- input]
 
-    let routeValues1 = solver m 30
+    let routeValues1 = solver graph 30
     print (maximum routeValues1)
     
-    let routeValues2 = solver m 26
-    print (maximum [ v+v1  | (k,v):ys <- tails (Map.assocs routeValues2), (k1,v1) <- ys, Set.null (Set.intersection k k1)])
+    let routeValues2 = solver graph 26
+    print (maximum [v1+v2 | (open1,v1) : elephants <- tails (Map.assocs routeValues2),
+                            (open2,v2) <- elephants,
+                            Set.null (Set.intersection open1 open2)])
 
 solver :: Map String (Int, [String]) -> Int -> Map (Set String) Int
-solver m = go [(("AA", Set.empty), 0)]
+solver graph = go [(("AA", Set.empty), 0)]
   where
     go states 0 = Map.fromListWith max [(open, n) | ((_, open), n) <- states]
     go states t = go (simplify (concatMap step states)) (t-1)
         where
             step ((here, open), n) =
-                [((next, open), n) | next <- snd (m Map.! here)] ++
+                [((next, open), n) | next <- snd (graph Map.! here)] ++
                 [((here, Set.insert here open), n + (t-1) * amt)
                     | Set.notMember here open
-                    , let amt = fst (m Map.! here), amt /= 0 ]
+                    , let amt = fst (graph Map.! here), amt /= 0 ]
     simplify = Map.assocs . Map.fromListWith max
