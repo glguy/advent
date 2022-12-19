7c517d4eac54ef5cfc35d71d01ed1d1615d28a8e
Eric Mertens <emertens@galois.com>
2022-12-19 09:29:23 -0800

    cleaner 19

--- solutions/src/2022/19.hs
+++ solutions/src/2022/19.hs
@@ -16,22 +16,20 @@ module Main where
 import Data.Set qualified as Set
 
 import Advent (format, ordNub)
-import Control.Concurrent.Async ( mapConcurrently )
+import Control.Parallel.Strategies ( parMap, rseq )
 
 type Blueprint = (Int, Int, Int, Int, Int, Int, Int)
 
+-- |
+-- >>> :main
+-- 1306
+-- 37604
 main :: IO ()
 main = do
     input <- [format|2022 19 (Blueprint %u: Each ore robot costs %u ore. Each clay robot costs %u ore. Each obsidian robot costs %u ore and %u clay. Each geode robot costs %u ore and %u obsidian.%n)*|]
-    xs <- mapConcurrently (\b@(i,_,_,_,_,_,_) ->
-            do let x = solve 24 b def
-               print (i,x)
-               return (i*x)) input
+    let xs = parMap rseq (\b@(i,_,_,_,_,_,_) -> i*solve 24 b def) input
     print (sum xs)
-    ys <- mapConcurrently (\b@(i,_,_,_,_,_,_) ->
-            do let x = solve 32 b def
-               print (i,x)
-               return x) (take 3 input)
+    let ys = parMap rseq (\b -> solve 32 b def) (take 3 input)
     print (product ys)
 
 solve :: Int -> Blueprint -> State -> Int
@@ -44,13 +42,13 @@ solve limit blue st0 = go 0 (Set.singleton (0,0,0,0)) [st0]
                 map earn sts ++
                 filter (\x -> rep x `Set.notMember` seen) (ordNub (concatMap (actions blue) sts))
 
+-- | Characterize the state by number of bots purchased
 rep :: State -> (Int,Int,Int,Int)
 rep st = (oreBots st, clayBots st, obsBots st, geodeBots st)
 
 data State = State {
     oreBots, clayBots, obsBots, geodeBots, ore, clay, obs, geo :: !Int
-}
-  deriving (Show, Eq, Ord)
+} deriving (Show, Eq, Ord)
 
 def :: State
 def = State {
@@ -60,30 +58,35 @@ def = State {
 actions :: Blueprint -> State -> [State]
 actions (_, oreCostOre, clayCostOre, obsCostOre, obsCostClay, geoCostOre, geoCostObs) st =
     [st' { ore = ore st' - geoCostOre, obs = obs st' - geoCostObs, geodeBots = geodeBots st' + 1 }
-        | geoCostOre <= ore st, geoCostObs <= obs st
-        ] +++
+        | geoCostOre <= ore st, geoCostObs <= obs st -- affordable
+        ] <++
     [st' { ore = ore st' - obsCostOre, clay = clay st' - obsCostClay, obsBots = obsBots st' + 1 }
-        | obsCostOre <= ore st, obsCostClay <= clay st
-        , obsBots st < obsBotMax] +++
-    [st' { ore = ore st' - clayCostOre, clayBots = clayBots st' + 1 } | clayCostOre <= ore st, clayBots st < clayBotMax] ++
-    [st' { ore = ore st' - oreCostOre, oreBots = oreBots st' + 1 } | oreCostOre <= ore st, oreBots st < oreBotMax]
+        | obsCostOre <= ore st, obsCostClay <= clay st -- affordable
+        , obsBots st < geoCostObs                      -- useful
+        ] <++
+    [st' { ore = ore st' - clayCostOre, clayBots = clayBots st' + 1 }
+        | clayCostOre <= ore st                        -- affordable
+        , clayBots st < obsCostClay                    -- useful
+        ] ++
+    [st' { ore = ore st' - oreCostOre, oreBots = oreBots st' + 1 }
+        | oreCostOre <= ore st                         -- affordable
+        , oreBots st < oreCostMax                      -- useful
+        ]
     where
         st' = earn st
-        oreBotMax = (oreCostOre `max` clayCostOre `max` obsCostOre `max` geoCostOre)
-        clayBotMax = obsCostClay
-        obsBotMax = geoCostObs
+        oreCostMax = (oreCostOre `max` clayCostOre `max` obsCostOre `max` geoCostOre)
 
 earn :: State -> State
 earn st =
-    st { ore  = ore st + oreBots st,
-         clay = clay st + clayBots st,
-         obs  = obs st + obsBots st,
-         geo = geo st + geodeBots st }
+    st { ore  = ore  st + oreBots   st,
+         clay = clay st + clayBots  st,
+         obs  = obs  st + obsBots   st,
+         geo  = geo  st + geodeBots st }
 
 -- I tried with this optimization and without it, and it runs faster preferring to buy ore and obs bots when possible
 -- but I doubt that it's always a good idea in general
-(+++) :: [a] -> [a] -> [a]
-[] +++ xs = xs
-xs +++ _ = xs
+(<++) :: [a] -> [a] -> [a]
+[] <++ xs = xs
+xs <++ _ = xs
 
-infixr 5 +++
+infixr 5 <++
