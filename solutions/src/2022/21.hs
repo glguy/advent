c50f07eb152edbee83ef5945022ef8222657af59
Eric Mertens <emertens@galois.com>
2022-12-21 09:19:34 -0800

    direct solution for 21

--- solutions/src/2022/21.hs
+++ solutions/src/2022/21.hs
@@ -1,4 +1,4 @@
-{-# Language QuasiQuotes, BlockArguments, ImportQualifiedPost #-}
+{-# Language DeriveDataTypeable, QuasiQuotes, BlockArguments, ImportQualifiedPost #-}
 {-|
 Module      : Main
 Description : Day 21 solution
@@ -11,19 +11,9 @@ Maintainer  : emertens@gmail.com
 -}
 module Main where
 
-import Data.Map (Map)
+import Data.Generics (Data, mkT, everywhere)
 import Data.Map qualified as Map
-import Data.SBV
-    ( sat,
-      sbvExists,
-      SInteger,
-      constrain,
-      SymVal(literal),
-      EqSymbolic((.==)),
-      SDivisible(sMod, sDiv),
-      Modelable(getModelValue),
-      SatResult )
-import Data.Foldable (traverse_)
+
 import Advent (format)
 
 type Input = [(String, Either Int (String, Char, String))]
@@ -36,8 +26,13 @@ main :: IO ()
 main =
  do input <- [format|2022 21 (%s: (%d|%s %c %s)%n)*|]
     print (part1 input)
-    result <- part2 input
-    traverse_ print (getModelValue "humn" result :: Maybe Integer)
+    let (l,r) = part2expr input
+    print
+      case (rewrite l, rewrite r) of
+        (x, Lit y) -> equal x y
+        (Lit x, y) -> equal y x
+        _ -> error "not supported"
+    
 
 part1 :: Input -> Int
 part1 xs = m Map.! "root"
@@ -51,22 +46,48 @@ part1 xs = m Map.! "root"
      eval (Right (a,'-',b)) = (m Map.! a) - (m Map.! b)
      eval _ = error "bad expression"
 
-part2 :: Input -> IO SatResult
-part2 xs = sat
- do humn <- sbvExists "humn"
-    let m :: Map String (SInteger)
-        m = Map.fromList [(k, if k == "humn" then humn else eval e) | (k,e) <- xs]
-        
-        eval (Left i) = literal (fromIntegral i) :: SInteger
-        eval (Right (a,'*',b)) = (m Map.! a) * (m Map.! b)
-        eval (Right (a,'+',b)) = (m Map.! a) + (m Map.! b)
-        eval (Right (a,'/',b)) = (m Map.! a) `sDiv` (m Map.! b)
-        eval (Right (a,'-',b)) = (m Map.! a) - (m Map.! b)
-        eval _ = error "bad expression"
-
-    sequence_ [constrain (sMod (m Map.! a) (m Map.! b) .== 0) | (_, Right(a,'/',b)) <- xs]
-    
+part2expr :: Input -> (Expr, Expr)
+part2expr xs =
     case lookup "root" xs of
-        Just (Right (r1,_,r2)) -> pure (m Map.! r1 .== m Map.! r2)
-        _ -> fail "missing root"
-    
+        Just (Right (r1,_,r2)) -> (m Map.! r1, m Map.! r2)
+        _ -> error "missing root"
+  where
+    m = Map.fromList [(k, if k == "humn" then Answer else eval e) | (k,e) <- xs] 
+    eval (Left i) = Lit i
+    eval (Right (a,'*',b)) = Mul (m Map.! a) (m Map.! b)
+    eval (Right (a,'+',b)) = Add (m Map.! a) (m Map.! b)
+    eval (Right (a,'/',b)) = Div (m Map.! a) (m Map.! b)
+    eval (Right (a,'-',b)) = Sub (m Map.! a) (m Map.! b)
+    eval _ = error "bad expression"
+
+
+constProp :: Expr -> Expr
+constProp (Add (Lit x) (Lit y)) = Lit (x+y)
+constProp (Sub (Lit x) (Lit y)) = Lit (x-y)
+constProp (Mul (Lit x) (Lit y)) = Lit (x*y)
+constProp (Div (Lit x) (Lit y)) | (z,0) <- x `quotRem` y = Lit z
+constProp e = e
+
+rewrite :: Expr -> Expr
+rewrite = everywhere (mkT constProp)
+
+equal :: Expr -> Int -> Int
+equal (Div x (Lit y)) z = equal x (y*z)
+equal (Add (Lit x) y) z = equal y (z-x)
+equal (Add x (Lit y)) z = equal x (z-y)
+equal (Mul (Lit x) y) z | (z',0) <- z `quotRem` x = equal y z'
+equal (Mul x (Lit y)) z | (z',0) <- z `quotRem` y = equal x z'
+equal (Sub (Lit x) y) z = equal y (x-z)
+equal (Sub x (Lit y)) z = equal x (y+z)
+equal Answer x = x
+equal x y = error ("stuck: " ++ show (x,y))
+
+data Expr
+    = Add Expr Expr
+    | Sub Expr Expr
+    | Mul Expr Expr
+    | Div Expr Expr
+    | Lit Int
+    | Answer
+    | Equal Expr Expr
+    deriving (Data, Show)
