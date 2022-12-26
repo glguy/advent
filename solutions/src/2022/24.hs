29010777b1062a97561d99530326e351d6dcab3a
Eric Mertens <emertens@galois.com>
2022-12-25 21:52:56 -0600

    comments

--- solutions/src/2022/24.hs
+++ solutions/src/2022/24.hs
@@ -25,22 +25,10 @@ module Main where
 
 import Data.Set (Set)
 import Data.Set qualified as Set
-import Data.Array.Unboxed
+import Data.Array.Unboxed (UArray, (!), bounds, inRange)
 
-import Advent ( getInputArray )
-import Advent.Coord (cardinal, west, Coord(..))
-
-checkCell :: UArray Coord Char -> Int -> Coord -> Bool
-checkCell w t here@(C y x) =
-  w!here /= '#' &&
-  w!(C y ((x'-t)`mod`xm+1)) /= '>' &&
-  w!(C y ((x'+t)`mod`xm+1)) /= '<' &&
-  w!(C ((y'-t)`mod`ym+1) x) /= 'v' &&
-  w!(C ((y'+t)`mod`ym+1) x) /= '^'
-  where
-    y' = y-1
-    x' = x-1
-    C ym xm = snd (bounds w) - 1
+import Advent (getInputArray)
+import Advent.Coord (Coord(..), cardinal, west)
 
 -- |
 -- >>> :main
@@ -49,26 +37,47 @@ checkCell w t here@(C y x) =
 main :: IO ()
 main =
  do input <- getInputArray 2022 24
-    let (_,hi) = bounds input
-    let target = hi + west
+    let start = C 0 1
+    let end   = snd (bounds input) + west
 
     let loop t end prev
           | Set.member end prev = t
           | otherwise = loop (t+1) end (grow input (t+1) prev)
 
-    let t1 = loop 0  target  (Set.singleton (C 0 1))
-        t2 = loop t1 (C 0 1) (Set.singleton target)
-        t3 = loop t2 target  (Set.singleton (C 0 1))
+    let t1 = loop 0  end   (Set.singleton start)
+        t2 = loop t1 start (Set.singleton end  )
+        t3 = loop t2 end   (Set.singleton start)
     print t1
     print t3
 
 -- | Given a set of locations the elf could be find the set the elf can be at next.
-grow :: UArray Coord Char -> Int -> Set Coord -> Set Coord
+grow ::
+  UArray Coord Char {- ^ input map          -} ->
+  Int               {- ^ time step          -} ->
+  Set Coord         {- ^ previous locations -} ->
+  Set Coord         {- ^ next locations     -}
 grow w t prev =
   Set.fromList
     [ next
       | here <- Set.toList prev
       , next <- here : cardinal here
       , inRange (bounds w) next
-      , checkCell w t next
+      , isOpen w t next
     ]
+
+-- | Check if a location in the world is available
+isOpen ::
+  UArray Coord Char {- ^ input map          -} ->
+  Int               {- ^ time step          -} ->
+  Coord             {- ^ location           -} ->
+  Bool              {- ^ location available -}
+isOpen w t here@(C y x) =
+  w ! here                    /= '#' &&
+  w ! (C y ((x'-t)`mod`xm+1)) /= '>' &&
+  w ! (C y ((x'+t)`mod`xm+1)) /= '<' &&
+  w ! (C ((y'-t)`mod`ym+1) x) /= 'v' &&
+  w ! (C ((y'+t)`mod`ym+1) x) /= '^'
+  where
+    y' = y-1
+    x' = x-1
+    C ym xm = snd (bounds w) - 1
