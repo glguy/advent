01a7edfae75b2e30b66a34193f82056aa9765ef0
Eric Mertens <emertens@galois.com>
2022-12-25 21:19:37 -0600

    faster 24

--- solutions/src/2022/24.hs
+++ solutions/src/2022/24.hs
@@ -30,7 +30,33 @@ import Data.Set (Set)
 import Data.Set qualified as Set
 
 import Advent ( getInputMap )
-import Advent.Coord
+import Advent.Coord ( cardinal, west, Coord(..) )
+
+data World = World {
+  stormsE, stormsW, stormsN, stormsS :: Map Int (Set Int),
+  region    :: Coord 
+} deriving (Show)
+
+parseWorld :: Map Coord Char -> World
+parseWorld m = World {
+  stormsE = Map.fromListWith Set.union [(y-1, Set.singleton (x-1)) | (C y x, '>') <- Map.assocs m],
+  stormsW = Map.fromListWith Set.union [(y-1, Set.singleton (x-1)) | (C y x, '<') <- Map.assocs m],
+  stormsN = Map.fromListWith Set.union [(x-1, Set.singleton (y-1)) | (C y x, '^') <- Map.assocs m],
+  stormsS = Map.fromListWith Set.union [(x-1, Set.singleton (y-1)) | (C y x, 'v') <- Map.assocs m],
+  region = maximum (Map.keys m)
+}
+
+checkBlizzard :: World -> Int -> Coord -> Bool
+checkBlizzard w t (C y x) =
+  seq w $
+  maybe False (Set.member ((x'-t)`mod`(xm-1))) (Map.lookup y' (stormsE w)) ||
+  maybe False (Set.member ((x'+t)`mod`(xm-1))) (Map.lookup y' (stormsW w)) ||
+  maybe False (Set.member ((y'-t)`mod`(ym-1))) (Map.lookup x' (stormsS w)) ||
+  maybe False (Set.member ((y'+t)`mod`(ym-1))) (Map.lookup x' (stormsN w))
+  where
+    y' = y-1
+    x' = x-1
+    C ym xm = region w
 
 -- |
 -- >>> :main
@@ -39,43 +65,27 @@ import Advent.Coord
 main :: IO ()
 main =
  do input <- Map.filter ('.' /=) <$> getInputMap 2022 24
-
+    let w = parseWorld input
     let corner = maximum (Map.keys input)
     let target = corner + west
 
-    let loop t best land
-          | Set.member land best = t
-          | otherwise =
-            loop (t+1) (grow corner world best) land
-              where world = worldAt corner (t+1) input
+    let loop t end prev
+          | Set.member end prev = t
+          | otherwise = loop (t+1) end (grow w (t+1) end prev)
 
-    let t1 = loop 0 (Set.singleton (C 0 1)) target
-        t2 = loop t1 (Set.singleton target) (C 0 1)
-        t3 = loop t2 (Set.singleton (C 0 1)) target
+    let t1 = loop 0  target  (Set.singleton (C 0 1))
+        t2 = loop t1 (C 0 1) (Set.singleton target)
+        t3 = loop t2 target  (Set.singleton (C 0 1))
     print t1
     print t3
 
--- | Find the set of obstructions in the world at a given time step
-worldAt :: Coord -> Int -> Map Coord Char -> Set Coord
-worldAt corner t input =
-  Set.fromList [ location corner k t v | (k,v) <- Map.toList input]
-
 -- | Given a set of locations the elf could be find the set the elf can be at next.
-grow :: Coord -> Set Coord -> Set Coord -> Set Coord
-grow corner world best =
+grow :: World -> Int -> Coord -> Set Coord -> Set Coord
+grow w t end prev =
   Set.fromList
     [ next
-      | here <- Set.toList best
-      , next@(C y x) <- here : cardinal here
-      , inRange (0,corner) next
-      , Set.notMember next world
+      | here <- Set.toList prev
+      , next <- here : cardinal here
+      , inRange (1,region w-1) next || next == here || next == end
+      , not (checkBlizzard w t next)
     ]
-
--- | Compute the location an obstacle will be at at a given time step
-location :: Coord -> Coord -> Int -> Char -> Coord
-location _ here _ '#' = here
-location corner here t c
-  | Just vec <- charToVec c =
-    zipCoord mod (here - 1 + scaleCoord t vec) (corner - 1) + 1
-location _ _ _ _ = undefined
-
