ca2cef615db73bf6027df0782d4537a383eb5f78
Eric Mertens <emertens@galois.com>
2022-12-25 22:10:05 -0600

    found a use for until

--- solutions/src/2022/24.hs
+++ solutions/src/2022/24.hs
@@ -30,6 +30,11 @@ import Data.Array.Unboxed (UArray, (!), bounds, inRange)
 import Advent (getInputArray)
 import Advent.Coord (Coord(..), cardinal, west)
 
+data SearchState = S {
+  time :: !Int,
+  world :: !(Set Coord)
+}
+
 -- |
 -- >>> :main
 -- 295
@@ -38,31 +43,35 @@ main :: IO ()
 main =
  do input <- getInputArray 2022 24
     let start = C 0 1
-    let end   = snd (bounds input) + west
-
-    let loop t end prev
-          | Set.member end prev = t
-          | otherwise = loop (t+1) end (grow input (t+1) prev)
-
-    let t1 = loop 0  end   (Set.singleton start)
-        t2 = loop t1 start (Set.singleton end  )
-        t3 = loop t2 end   (Set.singleton start)
+        end   = snd (bounds input) + west
+        t1    = shortest input 0  start end
+        t2    = shortest input t1 end   start
+        t3    = shortest input t2 start end
     print t1
     print t3
 
+-- | Find the shortest time from source to destination starting at a
+-- particular time step.
+shortest ::
+  UArray Coord Char {- ^ input map           -} ->
+  Int               {- ^ initial time step   -} ->
+  Coord             {- ^ starting location   -} ->
+  Coord             {- ^ ending location     -} ->
+  Int               {- ^ minimum travel time -}
+shortest input t src dst =
+    time (until (Set.member dst . world) (step input) (S t (Set.singleton src)))
+
 -- | Given a set of locations the elf could be find the set the elf can be at next.
-grow ::
-  UArray Coord Char {- ^ input map          -} ->
-  Int               {- ^ time step          -} ->
-  Set Coord         {- ^ previous locations -} ->
-  Set Coord         {- ^ next locations     -}
-grow w t prev =
+step ::
+  UArray Coord Char {- ^ input map -} ->
+  SearchState -> SearchState
+step w (S t prev) =
+  S (t+1) $
   Set.fromList
     [ next
       | here <- Set.toList prev
       , next <- here : cardinal here
-      , inRange (bounds w) next
-      , isOpen w t next
+      , isOpen w (t+1) next
     ]
 
 -- | Check if a location in the world is available
@@ -72,11 +81,12 @@ isOpen ::
   Coord             {- ^ location           -} ->
   Bool              {- ^ location available -}
 isOpen w t here@(C y x) =
-  w ! here                    /= '#' &&
-  w ! (C y ((x'-t)`mod`xm+1)) /= '>' &&
-  w ! (C y ((x'+t)`mod`xm+1)) /= '<' &&
-  w ! (C ((y'-t)`mod`ym+1) x) /= 'v' &&
-  w ! (C ((y'+t)`mod`ym+1) x) /= '^'
+  inRange (bounds w) here            &&
+  w ! here                    /= '#' && -- walls
+  w ! (C y ((x'-t)`mod`xm+1)) /= '>' && -- east-bound storms
+  w ! (C y ((x'+t)`mod`xm+1)) /= '<' && -- west-bound storms
+  w ! (C ((y'-t)`mod`ym+1) x) /= 'v' && -- south-bound storms
+  w ! (C ((y'+t)`mod`ym+1) x) /= '^'    -- north-bound storms
   where
     y' = y-1
     x' = x-1
