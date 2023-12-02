67f586fbcbab768a2fe7bc317c139a31b7434467
Eric Mertens <emertens@gmail.com>
2023-12-02 10:21:46 -0800

    tests

--- solutions/src/2023/02.hs
+++ solutions/src/2023/02.hs
@@ -8,9 +8,21 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2023/day/2>
 
+>>> :{
+:main + "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"
+:}
+8
+2286
+
 -}
 module Main where
 
+import Data.Map (Map)
 import Data.Map qualified as Map
 
 import Advent (format)
@@ -23,13 +35,18 @@ import Advent (format)
 main :: IO ()
 main =
  do input <- [format|2023 2 (Game %d: (%d %s)&(, )&(; )%n)*|]
-    print (sum [i | (i, gameSets) <- input, all (all part1) gameSets])
-    print (sum
-      [product (Map.fromListWith max [(s,n) | (n,s) <- concat gameSets])
-      | (_, gameSets) <- input])
+    let summaries = [(i, summarizeGame rounds) | (i, rounds) <- input]
+    print (sum [i | (i, summary) <- summaries, all part1 (Map.assocs summary)])
+    print (sum [product m | (_, m) <- summaries])
 
-part1 :: (Int, String) -> Bool
-part1 (v,k) =
-  case lookup k [("red", 12), ("green", 13), ("blue", 14)] of
-    Nothing -> False
-    Just n  -> n >= v
+summarizeGame :: [[(Int, String)]] -> Map String Int
+summarizeGame = Map.unionsWith max . map summarizeRound
+
+summarizeRound :: [(Int, String)] -> Map String Int
+summarizeRound r = Map.fromListWith (+) [(color, count) | (count, color) <- r]
+
+part1 :: (String, Int) -> Bool
+part1 ("red"  , v) = v <= 12
+part1 ("green", v) = v <= 13
+part1 ("blue" , v) = v <= 14
+part1 _            = False
