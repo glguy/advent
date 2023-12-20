4708f7b9f5c07f5d0ba4e0d74a4733ece38e04dd
Eric Mertens <emertens@galois.com>
2023-12-20 10:53:41 -0800

    cleanup

--- solutions/src/2023/20.hs
+++ solutions/src/2023/20.hs
@@ -1,4 +1,4 @@
-{-# Language QuasiQuotes, TemplateHaskell, BangPatterns, BlockArguments, LambdaCase, ImportQualifiedPost #-}
+{-# Language QuasiQuotes, TemplateHaskell, BangPatterns, LambdaCase, ImportQualifiedPost #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-|
 Module      : Main
@@ -15,7 +15,7 @@ and get a easier case like I did, but I assume we all got the same
 kind of dumb easy case as the last LCM problem this year.
 
 -}
-module Main where
+module Main (main) where
 
 import Advent (format, stageTH)
 import Advent.Queue as Queue
@@ -24,12 +24,16 @@ import Data.Map qualified as Map
 import Data.Set (Set)
 import Data.Set qualified as Set
 
--- | Kind of node in the graph
-data K
-  = K           -- ^ broadcast node
-  | K_PERCENT   -- ^ flip-flop
-  | K_AMPERSAND -- ^ conjunction gate
+-- | Kind sigil
+data K = K | K_PERCENT | K_AMPERSAND
   deriving (Eq, Ord, Show)
+
+data Kind
+  = Broadcast                -- ^ broadcast node
+  | Flipflop                 -- ^ flip-flop
+  | Conjunction (Set String) -- ^ conjunction gate
+
+data Stream a = a :| Stream a
 
 stageTH
 
@@ -41,55 +45,56 @@ stageTH
 main :: IO ()
 main =
  do input <- [format|2023 20 (@K%a+ -> %a+&(, )%n)*|]
-    let nodes = Map.fromList [(name, (kind, conns)) | (kind, name, conns) <- input]
-    let conj = Map.fromListWith (++) [(v, [k]) | (k, (_, vs)) <- Map.assocs nodes, v <- vs]    
-    
-    let part1 !n !l !h (("button",_,_):_) | n == (1000 :: Int) = l * h :: Int
-        part1 n l h ((src,_,sig):xs) =
-            part1 (if src == "button" then n+1 else n)
-                  (if sig then l else l+1)
-                  (if sig then h+1 else h) xs
-        part1 _ _ _ [] = error "part1 failed"
+    let incoming = Map.fromListWith (++) [(k, [v]) | (_, v, ks) <- input, k <- ks]
+    let nodes = Map.fromList [(name, (mkKind incoming name kind, conns)) | (kind, name, conns) <- input]
 
-    print (part1 0 0 0 (sim conj nodes))
-    
-    -- This is one of those sad days that you have to look at your own input :(
-    print (part2 conj (sim conj nodes))
+    print (part1 0 0 0 (sim nodes))
+    print (part2 incoming (sim nodes))
 
+mkKind :: Map String [String] -> String -> K -> Kind
+mkKind incoming name = \case
+  K           -> Broadcast
+  K_AMPERSAND -> Conjunction (Set.fromList [name ++ " " ++ x | x <- incoming Map.! name])
+  K_PERCENT   -> Flipflop
 
-part2 :: Map String [String] -> [(String, String, Bool)] -> Int
-part2 incoming msgs = foldl1 lcm [buttonsFor 0 dude msgs | dude <- incoming Map.! specialConj]
+part1 :: Int -> Int -> Int -> Stream (String, a, Bool) -> Int
+part1 n l h ((src,_,sig) :| xs)
+  | n == 1000, src == "button" = l * h
+  | otherwise =
+    part1 (if src == "button" then n+1 else n)
+          (if sig then l else l+1)
+          (if sig then h+1 else h) xs
+
+part2 :: Map String [String] -> Stream (String, String, Bool) -> Int
+part2 incoming msgs = foldl1 lcm [buttonsFor 0 gate msgs | gate <- incoming Map.! specialConj]
   where
     [specialConj] = incoming Map.! "rx"
 
-    buttonsFor n gate (("button", _, _):xs) = buttonsFor (n+1) gate xs
-    buttonsFor n gate ((src, dst, True):_) | src == gate, dst == specialConj = n
-    buttonsFor n gate (_:xs) = buttonsFor n gate xs
-    buttonsFor _ _ _ = undefined
+    buttonsFor n gate ((src, dst, msg) :| xs)
+      | "button" == src = buttonsFor (n+1) gate xs
+      | msg, src == gate, dst == specialConj = n
+    buttonsFor n gate (_ :| xs) = buttonsFor n gate xs
 
-sim :: Map String [String] -> Map String (K, [String]) -> [(String, String, Bool)]
-sim conj conns = go Set.empty Queue.Empty
+sim :: Map String (Kind, [String]) -> Stream (String, String, Bool)
+sim fwd = go Set.empty Queue.Empty
   where
-    go st q =
-      case Queue.pop q of
-        Nothing -> go st (Queue.singleton ("button", "broadcaster", False))
-        Just ((src, dst, msg), q') ->
-          (src, dst, msg) :
-          case Map.lookup dst conns of
-            Nothing -> go st q' -- output node, keep going
-            Just (K, next) ->
-              go st (appendList q' [(dst, t, msg) | t <- next])
-            Just (K_PERCENT, next)
-              | msg -> go st q' -- ignored
-              | otherwise -> -- was on sends low
-                  let active = not (Set.member dst st)
-                      outmsgs = [(dst, t, active) | t <- next]
-                  in go (mark dst active st) (appendList q' outmsgs)
-            Just (K_AMPERSAND, next) ->
-              let st1 = mark (src ++ " " ++ dst) msg st
-                  out = not (and [Set.member (inp ++ " " ++ dst) st1 | inp <- conj Map.! dst])
-                  outmsgs = [(dst, t, out) | t <- next]
-              in go st1 (Queue.appendList q' outmsgs)
+    go st Queue.Empty = go st (Queue.singleton ("button", "broadcaster", False))
+    go st ((src, dst, msg) Queue.:<| q') =
+      (src, dst, msg) :|
+      case Map.lookup dst fwd of
+        Just (Broadcast, next) -> continue st msg next -- forward message
+        Just (Flipflop, next)
+          | not msg -> continue st' out next -- was on sends low
+          where
+            st' = mark dst out st
+            out = not (Set.member dst st)
+        Just (Conjunction incoming, next) -> continue st' out next
+          where
+            st' = mark (dst ++ " " ++ src) msg st
+            out = not (incoming `Set.isSubsetOf` st')
+        _ -> go st q' -- ignored
+      where
+        continue st' msg' next = go st' (Queue.appendList q' [(dst, t, msg') | t <- next])
 
 mark :: Ord a => a -> Bool -> Set a -> Set a
 mark key True = Set.insert key
