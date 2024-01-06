3658dd0d66dd78e0816d168b0e82b85572b8dd31
Eric Mertens <emertens@gmail.com>
2024-01-05 20:47:37 -0800

    test cases

--- solutions/src/2021/08.hs
+++ solutions/src/2021/08.hs
@@ -10,11 +10,36 @@ Maintainer  : emertens@gmail.com
 
 Figure out how the miswired segment display works.
 
+>>> :{
+:main +
+"acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
+"
+:}
+0
+5353
+
+>>> :{
+:main +
+"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+"
+:}
+26
+61229
+
 -}
 module Main (main) where
 
 import Advent (countBy, format, fromDigits)
-import Data.Bits (Bits(setBit))
+import Data.Bits (setBit)
 import Data.Char (ord)
 import Data.List (permutations, foldl')
 import Data.Map (Map)
@@ -25,7 +50,7 @@ import Data.Map qualified as Map
 -- 983030
 main :: IO ()
 main =
- do inp <- [format|2021 8 (%s&  %| %s& %n)*|]
+ do inp <- [format|2021 8 ((%s )*%|( %s)*%n)*|]
     let outs = map solve inp
     print (countBy (`elem` [1,4,7,8]) (concat outs))
     print (sum (map (fromDigits 10) outs))
@@ -33,6 +58,27 @@ main =
 wires :: String
 wires = ['a'..'g']
 
+-- | Mapping from active line segments to digits using this arrangement.
+--
+-- @
+--   0:      1:      2:      3:      4:
+--  aaaa    ....    aaaa    aaaa    ....
+-- b    c  .    c  .    c  .    c  b    c
+-- b    c  .    c  .    c  .    c  b    c
+--  ....    ....    dddd    dddd    dddd
+-- e    f  .    f  e    .  .    f  .    f
+-- e    f  .    f  e    .  .    f  .    f
+--  gggg    ....    gggg    gggg    ....
+--
+--   5:      6:      7:      8:      9:
+--  aaaa    aaaa    aaaa    aaaa    aaaa
+-- b    .  b    .  .    c  b    c  b    c
+-- b    .  b    .  .    c  b    c  b    c
+--  dddd    dddd    ....    dddd    dddd
+-- .    f  e    f  .    f  e    f  .    f
+-- .    f  e    f  .    f  e    f  .    f
+--  gggg    gggg    ....    gggg    gggg
+-- @
 digits :: Map String Int
 digits = Map.fromList (zip ["abcefg","cf","acdeg","acdfg","bcdf","abdfg","abdefg","acf","abcdefg","abcdfg"] [0..9])
 
