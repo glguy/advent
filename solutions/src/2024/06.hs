9c642ebb72f368171764697b3649c14458c24a23
Eric Mertens <emertens@galois.com>
2024-12-06 08:20:19 -0800

    cleanup 2024-06

--- solutions/src/2024/06.hs
+++ solutions/src/2024/06.hs
@@ -8,6 +8,10 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2024/day/6>
 
+A guard walks around a grid. We check to see how long his route is and
+how many ways we can add an obstacle that will turn his route into a
+loop.
+
 >>> :{
 :main + "....#.....
 .........#
@@ -26,7 +30,8 @@ Maintainer  : emertens@gmail.com
 
 -}
 module Main (main) where
-import Advent (getInputArray, arrIx, ordNub)
+
+import Advent (getInputArray, arrIx, countBy, ordNub)
 import Advent.Coord (Coord, north, turnRight)
 import Data.Array.Unboxed (UArray, (//), assocs)
 
@@ -37,11 +42,10 @@ main :: IO ()
 main =
  do input <- getInputArray 2024 6
     let start = head [p | (p, '^') <- assocs input]
-    let path1 = ordNub (map snd (walk input north start))
+        path1 = ordNub (map snd (walk input north start))
+        check2 p = isLoop (walk (input // [(p,'#')]) north start)
     print (length path1)
-    print (length [() | p <- drop 1 path1 -- only bother checking the the initial path
-                       , let input' = input // [(p,'#')] -- place a wall
-                       , isLoop (walk input' north start)])
+    print (countBy check2 (drop 1 path1))
 
 -- | Generate the list of directions and positions generated walking from the
 -- starting point.
@@ -51,14 +55,16 @@ walk ::
     Coord             {- ^ position                        -} ->
     [(Coord, Coord)]  {- ^ list of direction and positions -}
 walk grid d p =
+    (d, p) :
     case arrIx grid (d + p) of
-        Nothing  -> [(d, p)]                    -- fell off
-        Just '#' -> walk grid (turnRight d) p   -- hit wall
-        _        -> (d,p) : walk grid d (d + p) -- open space
+        Nothing  -> []                        -- fell off
+        Just '#' -> walk grid (turnRight d) p -- hit wall
+        _        -> walk grid d (d + p)       -- moved
 
 -- | Predicate for paths that loop instead of running off the edge of the map.
+-- <https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_tortoise_and_hare>
 isLoop :: Ord a => [a] -> Bool
-isLoop xs = go xs xs
+isLoop a = go a a
   where
    go (x:xs) (_:y:ys) = x == y || go xs ys
    go _      _        = False
