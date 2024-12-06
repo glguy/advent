e14db423ead4b4f8057af4558118f1e84e9cedc7
Eric Mertens <emertens@galois.com>
2024-12-05 17:17:12 -0800

    Try out algebraic-graphs library

--- solutions/src/2024/05.hs
+++ solutions/src/2024/05.hs
@@ -45,7 +45,8 @@ Maintainer  : emertens@gmail.com
 module Main (main) where
 
 import Advent (format)
-import Data.Graph.Inductive (UGr, match, mkUGraph, subgraph, topsort)
+import Algebra.Graph.AdjacencyIntMap (edges, induce)
+import Algebra.Graph.AdjacencyIntMap.Algorithm (isTopSortOf, topSort)
 import Data.List (partition)
 
 -- | >>> :main
@@ -54,18 +55,12 @@ import Data.List (partition)
 main :: IO ()
 main =
  do (ords, pagess) <- [format|2024 5 (%u%|%u%n)*%n(%u&,%n)*|]
-    let graph       = mkUGraph (concat pagess) ords :: UGr
-        inputs      = [(pages, subgraph pages graph) | pages <- pagess]
-        (good, bad) = partition (uncurry topsorted) inputs
-    print (sum (map (middle .           fst) good))
-    print (sum (map (middle . topsort . snd) bad ))
+    let graph       = edges ords
+        inputs      = [(pages, induce (`elem` pages) graph) | pages <- pagess]
+        (good, bad) = partition (uncurry isTopSortOf) inputs
+    print (sum (map (middle . fst) good))
+    print (sum (map (either undefined middle . topSort . snd) bad))
 
 -- | Return the middle element of an odd-length list.
 middle :: [a] -> a
 middle xs = xs !! (length xs `div` 2)
-
--- | Check that node list is aleady topologically sorted.
-topsorted :: [Int] -> UGr -> Bool
-topsorted [] _ = True                        -- no pages left to check
-topsorted (p:ps) (match p -> (Just ([], _, _, _), g)) = topsorted ps g
-topsorted _ _ = False
