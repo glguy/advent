d30d6842a9f8ab4f8af3737b5581bad029f05763
Eric Mertens <emertens@gmail.com>
2024-12-15 23:33:40 -0800

    cleanup

--- solutions/src/2024/16.hs
+++ solutions/src/2024/16.hs
@@ -9,7 +9,7 @@ Maintainer  : emertens@gmail.com
 <https://adventofcode.com/2024/day/16>
 
 -}
-module Main where
+module Main (main) where
 
 import Advent (getInputArray)
 import Advent.Coord (Coord, east, turnRight, turnLeft)
@@ -25,37 +25,44 @@ main :: IO ()
 main =
  do input <- getInputArray 2024 16
     let start:_ = [p | (p,'S') <- assocs input]
-    let end  :_ = [p | (p,'E') <- assocs input]
-    let (p1, p2) = search input Set.empty (IntMap.singleton 0 (Map.singleton (start, east) (Set.singleton start)))
+        q0 = IntMap.singleton 0 (Map.singleton (start, east) (Set.singleton start))
+        (p1, p2) = search input Set.empty q0
     print p1
     print p2
 
 search ::
-  UArray Coord Char ->
-  Set (Coord, Coord) ->
-  IntMap (Map (Coord, Coord) (Set Coord)) ->
-  (Int, Int)
+  UArray Coord Char                       {- ^ input grid -} ->
+  Set (Coord, Coord)                      {- ^ position/velocity pairs already finished -} ->
+  IntMap (Map (Coord, Coord) (Set Coord)) {- ^ cost to (position/velocity to nodes-on-path -} ->
+  (Int, Int)                              {- ^ cost of shortest path and nodes on shorts paths -}
 search input seen q =
   case IntMap.minViewWithKey q of
-    Nothing -> undefined
+    Nothing -> error "no solution"
     Just ((cost, states), q1)
-      | not (null dones) -> (cost, length (mconcat dones))
-      | otherwise -> search input seen' q2
+      | not (null dones) -> (cost, Set.size (Set.unions dones))
+      | otherwise        -> search input seen' q2
       where
-        dones = [b | ((e,_),b) <- Map.assocs states1, input!e == 'E']
-        seen' = Set.union seen
-              $ Set.fromList (Map.keys states1)
-        states1 = Map.withoutKeys states seen
+        -- remove all the states at this cost that we've seen at a lower cost
+        states' = Map.withoutKeys states seen
+
+        -- look for states that have reached the target
+        dones = [visited | ((p, _), visited) <- Map.assocs states', input ! p == 'E']
+
+        -- mark all the new states at this cost as seen so we don't revisit them again
+        seen' = Set.union seen (Map.keysSet states')
+
+        -- queue up all the successor states to be visited in the future
         q2 = IntMap.unionWith merge q1
            $ IntMap.fromListWith merge
               [ next
-                | ((p,v), path) <- Map.assocs states1
-                , next <- [(cost+1000, Map.singleton (p, turnRight v) path)]
-                       ++ [(cost+1000, Map.singleton (p, turnLeft v) path)]
-                       ++ [(cost+1, Map.singleton (p+v, v) (Set.insert (p+v) path))
-                          | let c = input ! (p+v)
-                          , c == '.' || c == 'E']
+                | ((p, v), path) <- Map.assocs states'
+                , next <- [(cost + 1000, Map.singleton (p, turnRight v) path)]
+                       ++ [(cost + 1000, Map.singleton (p, turnLeft  v) path)]
+                       ++ [(cost +    1, Map.singleton (p', v) (Set.insert p' path))
+                          | let p' = p + v, '#' /= input ! p'
+                          ]
               ]
+
         merge = Map.unionWith Set.union
            
   
