d61201ec6dbd63a08204ddf537a484786eb5119b
Eric Mertens <emertens@gmail.com>
2025-11-30 21:40:03 -0800

    simpler

--- solutions/src/2025/01.hs
+++ solutions/src/2025/01.hs
@@ -12,6 +12,7 @@ Maintainer  : emertens@gmail.com
 module Main (main) where
 
 import Advent (count, format, stageTH)
+import Data.List (mapAccumL)
 
 data D = DL | DR deriving Show
 
@@ -21,16 +22,16 @@ main :: IO ()
 main =
  do input <- [format|2025 1 (@D%d%n)*|]
     print (count 0 (scanl step1 50 input))
-    print (snd (foldl sim2 (50, 0) input))
+    print (sum (snd (mapAccumL sim2 50 input)))
 
 step1 :: Int -> (D, Int) -> Int
 step1 x (DR, n) = (x + n) `mod` 100
 step1 x (DL, n) = (x - n) `mod` 100
 
-sim2 :: (Int, Int) -> (D, Int) -> (Int, Int)
-sim2 (here, acc) (DR, n) = (here', acc + zeros)
+sim2 :: Int -> (D, Int) -> (Int, Int)
+sim2 here (DR, n) = (here', zeros)
   where (zeros, here') = (here + n) `divMod` 100
-sim2 (here, acc) (DL, n) = (sym here', acc + zeros)
+sim2 here (DL, n) = (sym here', zeros)
   where (zeros, here') = (sym here + n) `divMod` 100
 
 sym :: Int -> Int
