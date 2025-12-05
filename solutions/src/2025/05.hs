b8d31c1ec9e042b69df617695816bd98f8cd5e21
Eric Mertens <emertens@gmail.com>
2025-12-04 21:23:55 -0800

    2025-05

---
+++ solutions/src/2025/05.hs
@@ -0,0 +1,47 @@
+{-# Language QuasiQuotes, GADTs #-}
+{-|
+Module      : Main
+Description : Day 5 solution
+Copyright   : (c) Eric Mertens, 2025
+License     : ISC
+Maintainer  : emertens@gmail.com
+
+<https://adventofcode.com/2025/day/5>
+
+>>> :{
+:main +
+"3-5
+10-14
+16-20
+12-18
+\&
+1
+5
+8
+11
+17
+32
+"
+:}
+3
+14
+
+-}
+module Main where
+
+import Advent (countBy, format)
+import Advent.Box (size, unionBoxes, Box(Pt, Dim))
+
+-- | >>> :main
+-- 664
+-- 350780324308385
+main :: IO ()
+main =
+ do (fresh, input) <- [format|2025 5 (%u-%u%n)*%n(%u%n)*|]
+
+    -- NB. Dim uses an exclusive upper-bound (so we add 1)
+    let fresh' = unionBoxes [Dim lo (hi + 1) Pt | (lo, hi) <- fresh]
+        isFresh x = any (\(Dim lo hi Pt) -> lo <= x && x < hi) fresh'
+
+    print (countBy isFresh input)
+    print (sum (map size fresh'))
